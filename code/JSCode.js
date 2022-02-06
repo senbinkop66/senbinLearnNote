1b123045a43294d1c6a4ab2787b6877bbf5caa0f
senbinkop66 <2507019430@qq.com>
2022-02-06 12:49:06 +0800

    update

--- code/JSCode.js
+++ code/JSCode.js
@@ -236,3 +236,54 @@ var addTwoNumbers = function(l1, l2) {
 
 };
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let n=nums.length>>1;
+    //console.log(n);
+    let obj={};
+    for (let i=0;i<nums.length;i++){
+        obj[nums[i]]===undefined ? obj[nums[i]]=1 : ++obj[nums[i]];
+    }
+    //console.log(obj);
+    let result;
+    Object.keys(obj).forEach((key)=>{
+        if (obj[key]>n) {
+            result=key;
+        }
+    });
+    return result;
+
+};
+
+let test=[2,2,1,1,1,2,2];
+let result=majorityElement(test);
+console.log(result);
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function(nums) {
+    let obj={};
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        obj[nums[i]]===undefined ? obj[nums[i]]=1 : ++obj[nums[i]];
+        if (obj[nums[i]]===1) {
+            //计数为1时，加该值
+            sum+=nums[i];
+        }
+        if (obj[nums[i]]===2) {
+            //计数为2时,减掉
+            sum-=nums[i];
+        }
+    }
+    return sum;
+};
+
+
+let test=[2,2,1,1,1,2,2];
+let result=sumOfUnique(test);
+console.log(result);
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,63 +1,25 @@
 /**
- * @param {string} a
- * @param {string} b
+ * @param {number[]} nums
  * @return {number}
  */
-var repeatedStringMatch = function(a, b) {
-    //首先只要b中有字母不在a中,则不可能b为a的子串
-    let len_a=a.length;
-    let len_b=b.length;
-
-    let set=new Set();
-    for (let i=0;i<len_a;i++){
-        set.add(a[i]);
-    }
-    for (let i=0;i<len_b;i++){
-        if (!set.has(b[i])) {
-            return -1;
+var sumOfUnique = function(nums) {
+    let obj={};
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        obj[nums[i]]===undefined ? obj[nums[i]]=1 : ++obj[nums[i]];
+        if (obj[nums[i]]===1) {
+            //计数为1时，加该值
+            sum+=nums[i];
+        }
+        if (obj[nums[i]]===2) {
+            //计数为2时,减掉
+            sum-=nums[i];
         }
     }
-    let newStr=a;
-    //判断b与a的长度，a当重复后长度最多超过b长度的2倍，如果2倍长度没有找到则返回-1
-    let n=len_b%len_a===0 ? len_b/len_a : Math.floor(len_b/len_a)+1;
-    if (len_a>=len_b) {
-        //当a长时，返回值最多为2
-        if (newStr.indexOf(b)!==-1) {
-            return 1;
-        }else{
-            newStr+=a;
-            if (newStr.indexOf(b)!==-1) {
-                return 2;
-            }else{
-                return -1;
-            }
-        }
-    }else{
-        //当b长时，a长度最多增加到b的2倍
-        for (let i=0;i<n-1;i++){
-            newStr+=a;
-        }
-        while (newStr.length<2*len_b){
-            if (newStr.indexOf(b)!==-1) {
-                return n;
-            }else{
-                newStr+=a;
-                n++;
-            }
-        }
-        if (newStr.indexOf(b)!==-1) {
-            return n;
-        }else{
-            return -1;
-        }
-        
-    }
-    return 0;
+    return sum;
 };
 
 
-let str1 = "abc";
-let str2="cabcabca";
-
-let result=repeatedStringMatch(str1,str2);
+let test=[2,2,1,1,1,2,2];
+let result=sumOfUnique(test);
 console.log(result);
