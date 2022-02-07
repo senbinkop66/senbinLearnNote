1c7ebd12636bd2a00b2ff4a3d614a1c124c56e0c
senbinkop66 <2507019430@qq.com>
2022-02-07 13:05:18 +0800

    update

--- code/JSCode.js
+++ code/JSCode.js
@@ -329,5 +329,43 @@ var reverseBits = function(n) {
     return rev >>> 0;
 };
 
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {string}
+ */
+var longestDiverseString = function(a, b, c) {
+    let result=[];
+    const arr=[[a,'a'],[b,'b'],[c,'c']];
+    while(true){
+        arr.sort((a,b)=>b[0]-a[0]);
+        let hasNext=false;
+        for(const [i,[c,ch]] of arr.entries()){
+            if (c<=0) {
+                break;
+            }
+            const m=result.length;
+            if (m>=2 && result[m-2]===ch && result[m-1]===ch) {
+                continue;
+            }
+            hasNext=true;
+            result.push(ch);
+            arr[i][0]--;
+            break;
+        }
+        if (!hasNext) {
+            break;
+        }
+    }
+    return result.join("");
+};
+
+
+
+let test=100;
+let result=longestDiverseString(1,1,7);
+console.log(result);
+
 
 
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,20 +1,38 @@
 /**
- * @param {number} n - a positive integer
- * @return {number}
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {string}
  */
-var hammingWeight = function(n) {
-    let ret=0;
-    while(n){
-        //因为每次运算会使得 nn 的最低位的 11 被翻转，因此运算次数就等于 nn 的二进制位中 11 的个数。
-        n&=n-1;
-        ret++;
+var longestDiverseString = function(a, b, c) {
+    let result=[];
+    const arr=[[a,'a'],[b,'b'],[c,'c']];
+    while(true){
+        arr.sort((a,b)=>b[0]-a[0]);
+        let hasNext=false;
+        for(const [i,[c,ch]] of arr.entries()){
+            if (c<=0) {
+                break;
+            }
+            const m=result.length;
+            if (m>=2 && result[m-2]===ch && result[m-1]===ch) {
+                continue;
+            }
+            hasNext=true;
+            result.push(ch);
+            arr[i][0]--;
+            break;
+        }
+        if (!hasNext) {
+            break;
+        }
     }
-    return ret;
+    return result.join("");
 };
 
 
 
 let test=100;
-let result=hammingWeight(test);
+let result=longestDiverseString(1,1,7);
 console.log(result);
 
