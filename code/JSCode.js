2bb7e7fa5152255ee59e13c901435282da25d96d
senbinkop66 <2507019430@qq.com>
2022-02-08 21:58:02 +0800

    update

--- code/JSCode.js
+++ code/JSCode.js
@@ -552,3 +552,44 @@ let queries=[[0,0],[0,0]];
 let result=gridIllumination(n,lamps,queries);
 console.log(result);
 
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set=new Set();
+    for(let i=0;i<nums.length;i++){
+        if (set.has(nums[i])) {
+            return true;
+        }else{
+            set.add(nums[i]);
+        }
+    }
+    return false;
+};
+
+let test=[1,1,1,3,3,4,3,2,4,2];
+let result=containsDuplicate(test);
+console.log(result);
+
+var containsNearbyDuplicate = function(nums, k) {
+    let m=new Map();
+    for(let i=0;i<nums.length;i++){
+        if (m.has(nums[i])) {
+            if (Math.abs(m.get(nums[i])-i)<=k) {
+                return true;
+            }else{
+                m.set(nums[i],i);
+            }
+        }else{
+            m.set(nums[i],i);
+        }
+        //console.log(m);
+    }
+    return false;
+};
+
+let test=[1,0,1,1];
+let result=containsNearbyDuplicate(test,1);
+console.log(result);
+
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,79 +1,25 @@
 /**
- * @param {number} n
- * @param {number[][]} lamps
- * @param {number[][]} queries
- * @return {number[]}
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
  */
-var gridIllumination = function(n, lamps, queries) {
-    let grids=new Array(n);
-    for (let i=0;i<n;i++){
-        grids[i]=new Array(n).fill(0);
-    }
-    //N,NE,E,ES,S,SE,E,EN
-    const dirs=[[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
-    lamps.forEach((lamp)=>{
-        if (grids[lamp[0]][lamp[1]]<4*(n-1)+1) {
-            //避免灯重复变亮
-            dirs.forEach((dir)=>{
-                let row=lamp[0];
-                let col=lamp[1];
-                while(row>=0 && row<n && col>=0 && col<n ){
-                    grids[row][col]+=1;
-                    row+=dir[0];
-                    col+=dir[1];
-                }
-            });
-            grids[lamp[0]][lamp[1]]+=4*(n-1)+1;
-        }
-    });
-    //console.log(grids);
-    const result=[];
-    queries.forEach((query)=>{
-        if (grids[query[0]][query[1]]>0) {
-            result.push(1);
-        }else{
-            result.push(0);
-        }
-        if (grids[query[0]][query[1]]>4*(n-1)) {
-            //查询的灯亮着
-            //关闭该灯并设置被照明的网格-1
-            shutTheLamp(grids,query[0],query[1],n);
-            //console.log(grids);
-        }
-        dirs.forEach((dir)=>{
-            let row=query[0]+dir[0];
-            let col=query[1]+dir[1];
-            if(row>=0 && row<n && col>=0 && col<n ){
-                if (grids[row][col]>4*(n-1)) {
-                    //查询灯其他方向上位置的灯亮着
-                    //关闭该灯并设置被照明的网格-1
-                    shutTheLamp(grids,row,col,n);
-                    //console.log(grids);
-                }
+var containsNearbyDuplicate = function(nums, k) {
+    let m=new Map();
+    for(let i=0;i<nums.length;i++){
+        if (m.has(nums[i])) {
+            if (Math.abs(m.get(nums[i])-i)<=k) {
+                return true;
+            }else{
+                m.set(nums[i],i);
             }
-        });
-    });
-    return result;
-};
-
-var shutTheLamp=function(grids,row0,col0,n){
-    const dirs=[[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]];
-    dirs.forEach((dir)=>{
-        let row=row0;
-        let col=col0;
-        while(row>=0 && row<n && col>=0 && col<n ){
-            grids[row][col]-=1;
-            row+=dir[0];
-            col+=dir[1];
+        }else{
+            m.set(nums[i],i);
         }
-    });
-    grids[row0][col0]-=(4*(n-1)+1);
+        //console.log(m);
+    }
+    return false;
 };
 
-let n = 1;
-//let lamps =[[2,5],[4,2],[0,3],[0,5],[1,4],[4,2],[3,3],[1,0]];
-//let queries = [[4,3],[3,1],[5,3],[0,5],[4,4],[3,3]];
-let lamps=[[0,0],[0,0]];
-let queries=[[0,0],[0,0]];
-let result=gridIllumination(n,lamps,queries);
+let test=[1,0,1,1];
+let result=containsNearbyDuplicate(test,1);
 console.log(result);
