7568dde16053d4312b78c2e139bf57f76c938398
senbinkop66 <2507019430@qq.com>
2022-02-15 22:31:12 +0800

    update

--- code/JSCode.js
+++ code/JSCode.js
@@ -1377,3 +1377,36 @@ let test=[3,3,7,7,10,11,11];
 let result=singleNonDuplicate(test);
 console.log(result);
 
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var luckyNumbers  = function(matrix) {
+     let m=matrix.length;
+     let n=matrix[0].length;
+     const maxColArr=new Array(n).fill(0);;  //每一列的最大值
+     const minRowArr=new Array(m).fill(0);  //每一行的最大值索引;
+     for(let i=0;i<m;i++){
+          for(let j=0;j<n;j++){
+               maxColArr[j]=Math.max(maxColArr[j],matrix[i][j]);
+               //矩阵中的所有元素都是不同的
+               minRowArr[i]=matrix[i][minRowArr[i]]<matrix[i][j] ? minRowArr[i] : j;
+          }
+     }
+     //console.log(matrix);
+     //console.log(maxColArr);
+     //console.log(minRowArr);
+     let result=[];
+     for (let i=0;i<m;i++){
+          if (matrix[i][minRowArr[i]]===maxColArr[minRowArr[i]]) {
+               result.push(matrix[i][minRowArr[i]]);
+          }
+     }
+     return result;
+
+};
+
+let test=[[1,10,4,2],[9,3,8,7],[15,16,17,12]];
+let result=luckyNumbers(test);
+console.log(result);
+
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,22 +1,32 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * @param {number[][]} matrix
+ * @return {number[]}
  */
-var singleNonDuplicate = function(nums) {
-     //数组长度一定是奇数个
-     let left=0;
-     let right=nums.length-1;
-     while(left<right){
-          let mid=(left+right)>>1;
-          if (nums[mid]===nums[mid^1]) {
-               left=mid+1;
-          }else{
-               right=mid;
+var luckyNumbers  = function(matrix) {
+     let m=matrix.length;
+     let n=matrix[0].length;
+     const maxColArr=new Array(n).fill(0);;  //每一列的最大值
+     const minRowArr=new Array(m).fill(0);  //每一行的最大值索引;
+     for(let i=0;i<m;i++){
+          for(let j=0;j<n;j++){
+               maxColArr[j]=Math.max(maxColArr[j],matrix[i][j]);
+               //矩阵中的所有元素都是不同的
+               minRowArr[i]=matrix[i][minRowArr[i]]<matrix[i][j] ? minRowArr[i] : j;
           }
      }
-     return nums[left];
+     //console.log(matrix);
+     //console.log(maxColArr);
+     //console.log(minRowArr);
+     let result=[];
+     for (let i=0;i<m;i++){
+          if (matrix[i][minRowArr[i]]===maxColArr[minRowArr[i]]) {
+               result.push(matrix[i][minRowArr[i]]);
+          }
+     }
+     return result;
+
 };
 
-let test=[3,3,7,7,10,11,11];
-let result=singleNonDuplicate(test);
+let test=[[1,10,4,2],[9,3,8,7],[15,16,17,12]];
+let result=luckyNumbers(test);
 console.log(result);
