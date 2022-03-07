ff4d0ba38b9b57dc3ff17cf25aefb87de6bdaf75
senbinkop66 <2507019430@qq.com>
2022-03-07 21:14:55 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,38 +1,25 @@
-var Iterator=function(obj){
-   var current=0;
-   var next=function(){
-      current+=1;
-   };
-   var isDone=function(){
-      return current>=obj.length;
-   };
-   var getCurrItem=function(){
-      return obj[current];
-   };
-   return {
-      next:next,
-      isDone:isDone,
-      getCurrItem:getCurrItem,
-      length:obj.length
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+   if (num===0) {
+      return "0";
    }
+   let flag=false;
+   if (num<0) {
+      flag=true;
+   }
+   num=Math.abs(num);
+   let ans=[];
+   while(num>0){
+      let e=num%7;
+      num=(num-e)/7;
+      ans.unshift(e);
+   }
+   return flag? "-"+ans.join("") : ans.join("");
 };
 
-var compare = function(iterator1,iterator2){
-   if ( iterator1.length !== iterator2.length ){
-      console.log( 'iterator1 和 iterator2 不相等' );
-      return;
-   }
-   while(!iterator1.isDone() && !iterator2.isDone()){
-      if (iterator1.getCurrItem()!==iterator2.getCurrItem()) {
-         console.log( 'iterator1 和 iterator2 不相等' );
-         return;
-      }
-      iterator1.next();
-      iterator2.next();
-   }
-   console.log ( 'iterator1 和 iterator2 相等' );
-};
-
-var iterator1 = Iterator( [ 1, 2, 3 ,4] );
-var iterator2 = Iterator( [ 1, 2, 3 ,5] );
-compare( iterator1, iterator2 ); 
+let num = -7;
+let result=convertToBase7(num);
+console.log(result);
