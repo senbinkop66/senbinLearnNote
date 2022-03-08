a2b83360156c07c8c8eb591e7b0826030cb9e2d1
senbinkop66 <2507019430@qq.com>
2022-03-08 19:41:26 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,25 +1,40 @@
 /**
- * @param {number} num
- * @return {string}
+ * @param {string} s
+ * @param {number[][]} queries
+ * @return {number[]}
  */
-var convertToBase7 = function(num) {
-   if (num===0) {
-      return "0";
+var platesBetweenCandles = function(s, queries) {
+   let ans=new Array(queries.length).fill(0);
+   let n=s.length;
+   const preSum=new Array(n).fill(0);
+   for(let i=0,sum=0;i<n;i++){
+      if (s[i]==="*") {
+         sum++;
+      }
+      preSum[i]=sum;
    }
-   let flag=false;
-   if (num<0) {
-      flag=true;
+   const left=new Array(n).fill(0);
+   for(let i=0,l=-1;i<n;i++){
+      if (s[i]==="|") {
+         l=i;
+      }
+      left[i]=l;
    }
-   num=Math.abs(num);
-   let ans=[];
-   while(num>0){
-      let e=num%7;
-      num=(num-e)/7;
-      ans.unshift(e);
+   const right=new Array(n).fill(0);
+   for(let i=n-1,r=-1;i>=0;i--){
+      if (s[i]==="|") {
+         r=i;
+      }
+      right[i]=r;
    }
-   return flag? "-"+ans.join("") : ans.join("");
+   for(let i=0;i<queries.length;i++){
+      let x=right[queries[i][0]];
+      let y=left[queries[i][1]];
+      ans[i]=x===-1 || y===-1 || x>=y ? 0: preSum[y]-preSum[x];
+   }
+   return ans;
 };
 
-let num = -7;
-let result=convertToBase7(num);
+let s = "**|**|***|", queries = [[2,5],[5,9]];
+let result=platesBetweenCandles(s,queries);
 console.log(result);
