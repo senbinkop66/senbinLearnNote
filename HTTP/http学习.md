4baa891aff59b97a0553fedc376a8924ea0995ce
senbinkop66 <2507019430@qq.com>
2022-03-24 22:07:03 +0800

    update

--- HTTP/http学习.md
+++ HTTP/http学习.md
@@ -475,3 +475,47 @@ MIME类型不是传达文档类型信息的唯一方式：
 
 # 选择 www 或非 www URL 作为域名
 
+网站所有者经常会问的一个问题是选择非 www 的还是 www 的网址。本文提供了选择建议。
+
+## [什么是域名？](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/Choosing_between_www_and_non-www_URLs#什么是域名？)
+
+在一个 HTTP 网址中，在初始 `http://` 或 `https://` 后的第一个子字符串称为域。它是文档所在的服务器的名称。
+
+一个服务器不一定是一个独立的物理机：几台服务器可以驻留在同一台物理机器上，或者一台服务器可以通过几台机器进行处理，协作处理并响应或负载均衡它们之间的请求。关键点在于语义上**一个域名代表一个单独的服务器**。
+
+## [所以，我只能选择其中一个做为我的网站的网址？](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/Choosing_between_www_and_non-www_URLs#所以，我只能选择其中一个做为我的网站的网址？)
+
+- 是的，你必须选择其中之一，且一直使用。选择使用其中哪一个取决于你，当你选择完之后，就要保持下去。这样，无论是对用户，还是对搜索引擎，你的网站能保持更好的一致性。这包括始终链接到所选域名（如果你在网站中使用相对网址，则不应该很难），也可以始终将链接（通过电子邮件/社交网络等）共享使用同一个域名。
+- 不，你可以有两个。重要的是，你需是保持的那一个官方的域名，**这个官方域名被称为\*规范\*名称**。你所有的绝对链接应该使用它。但即便如此，你仍然可以有其他域名使用：HTTP允许使用两种技术，以便它在使用规范域名的同时还允许非规范域名使用，使使用者或搜索引擎可以准确的访问到所预期的页面。
+
+所以，选择其中一个作为你的域名的规范地址！下面有两种技术允许不规范的域名仍然起作用。
+
+## [规范网址方式](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/Choosing_between_www_and_non-www_URLs#规范网址方式)
+
+选择下面有两种不同的方式使网站*规范*。
+
+### [使用 HTTP 301 重定向](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/Choosing_between_www_and_non-www_URLs#使用_http_301_重定向)
+
+在这种情况下，你需要配置服务器接收的HTTP请求（ 常见为 www 和非 www 网址相同）以及适当的HTTP响应 [`301`](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Status/301) 去响应所有非规范的域名请求。这会将尝试使访问非规范网址的浏览器重定向到其规范的等效网址。举例来说，如果您选择使用非 www 网址为规范类型，你的所有 www 网址都应该被重定向到对应的非 www 网址上。
+
+例如：
+
+1. 服务器收到 http://www.example.org/whaddup 请求（当规范域名是 example.org 时）
+2. 服务器则以代码 [`301`](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Status/301) 与头 [`Location`](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Location) ：`http://example.org/whaddup`
+3. 该客户端发出的规范的域名请求：http://example.org/whaddup
+
+它可以将一个特殊的 HTML [``](https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element/link) 元素添加到网页指示什么网页的标准地址，这对页面的访问者没有影响，但在搜索引擎检索时会告诉搜索引擎当页面实际的地址。通过这种方式，搜索引擎不需要索引同一页面多次，那样可能导致它被视为重复的内容或垃圾邮件，甚至从搜索引擎结果中删除或者降低你的页面显示排名。
+
+当加入这样一个标签，会告诉搜索引擎，你提供相同内容的两个域名那一个是规范的。以前面的例子为例，http://www.example.org/whaddup 将提供与 http://example.org/whaddup 相同的内容，但有一个附加的 [``](https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element/link) 头部元素：
+
+```
+<link href="http://example.org/whaddup" rel="canonical">
+```
+
+不同于以往，浏览器历史记录将考虑非 www 和 www 的网址作为独立的条目。
+
+## [使你页面都有效](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/Choosing_between_www_and_non-www_URLs#使你页面都有效)
+
+有了这些技术，你可以配置服务器对www前缀和非www前缀的域名进行正确的响应，如果你无法预测用户会在浏览器地址栏使用哪个URL，你需要做的就是选择一个作为你的规范地址，然后重定向另一个。
+
+# HTTP 基础
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,128 +1,26 @@
-function Player(name,teamColor){
-    this.state = 'alive'; // 玩家状态
-    this.name = name; // 角色名字
-    this.teamColor = teamColor; // 队伍颜色
-};
-Player.prototype.win=function(){
-    console.log( this.name + ' won ' );
-};
-Player.prototype.lose=function(){
-    console.log( this.name +' lost' );
-};
+var Plane=function(){}
 
-Player.prototype.die=function(){
-    this.state = 'dead';
-    playerDirector.reciveMessage('playerDead', this ); // 给中介者发送消息，玩家死亡
-};
-Player.prototype.remove=function(){
-    playerDirector.reciveMessage("removePlayer",this);  // 给中介者发送消息，移除一个玩家
-};
-Player.prototype.changeTeam=function(color){
-    playerDirector.reciveMessage("changeTeam",this,color);  //给中介者发送消息，玩家换队
+Plane.prototype.fire=function(){
+    console.log( '发射普通子弹' );
 }
 
-//最后定义一个工厂来创建玩家：
-var playerFactory=function(name,teamColor){
-    var newPlayer=new Player(name,teamColor);  //创建新玩家
-    playerDirector.reciveMessage("addPlayer",newPlayer);  //给中介者发送消息，新增玩家
-    return newPlayer;
+//增加两个装饰类，分别是导弹和原子弹：
+var MissileDecorator=function(plane){
+    this.plane=plane;
+};
+MissileDecorator.prototype.fire=function(){
+    this.plane.fire();
+    console.log( '发射导弹' );
+};
+var AtomDecorator = function( plane ){
+    this.plane = plane;
+}
+AtomDecorator.prototype.fire = function(){
+    this.plane.fire();
+    console.log( '发射原子弹' );
 }
 
-var playerDirector=(function(){
-    var players={};  //保存所有玩家
-    var operations={};  // 中介者可以执行的操作
-
-    operations.addPlayer=function(player){
-        //新增一个玩家
-        var teamColor=player.teamColor;  //玩家的队伍颜色
-        players[teamColor]=players[teamColor] || [];  // 如果该颜色的玩家还没有成立队伍，则新成立一个队伍
-        players[teamColor].push(player);  //添加玩家进队伍
-    };
-
-    operations.removePlayer=function(player){
-        //移除一个玩家
-        var teamColor=player.teamColor;
-        var teamPlayers=players[teamColor] || [];  //该队伍所有成员
-        for (let i=teamPlayers.length-1;i>=0;i--){
-            //遍历删除
-            if (teamPlayers[i]===player) {
-                teamPlayers.splice(i,1);
-            }
-        }
-    };
-
-    operations.changeTeam=function(player,newTeamColor){
-        //玩家换队
-        operations.removePlayer(player);  //从原队伍中删除
-        player.teamColor=newTeamColor;  //改变队伍颜色
-        operations.addPlayer(player);  //增加到新队伍中
-    };
-
-    operations.playerDead=function(player){
-        //玩家死亡
-        var teamColor=player.teamColor;
-        teamPlayers=players[teamColor];  //玩家所在队伍
-
-        var all_dead=true;
-
-        for (let i=0;i<teamPlayers.length;i++){
-            //遍历队友列表
-            if (teamPlayers[i].state!=="dead") {
-                //如果还有一个队友没有死亡，则游戏还未失败
-                all_dead=false;
-                break;
-            }
-        }
-        if (all_dead===true) {  // 如果队友全部死亡
-            for (let i=0;i<teamPlayers.length;i++){
-                // 通知所有队友玩家游戏失败
-                teamPlayers[i].lose();  //本队所有玩家 lose
-            }
-            for(let color in players){
-                if (color!==teamColor) {
-                    var teamPlayers = players[ color ];  // 其他队伍的玩家
-                    for (let i=0;i<teamPlayers.length;i++){
-                        teamPlayers[i].win();  // 其他队伍所有玩家 win
-                    }
-                }
-            }
-            
-        }
-    };
-
-    var reciveMessage=function(){
-        var message=Array.prototype.shift.call(arguments);  //arguments 的第一个参数为消息名称
-        operations[message].apply(this,arguments);
-    };
-
-    return {reciveMessage:reciveMessage}
-})();
-
-
-
-var player1=playerFactory("Mane","red");
-var player2=playerFactory("Alison","red");
-var player3=playerFactory("Arnold","red");
-var player4=playerFactory("Salah","red");
-
-var player5=playerFactory("mendy","blue");
-var player6=playerFactory("kaipa","blue");
-var player7=playerFactory("lukaku","blue");
-var player8=playerFactory("mount","blue");
-/*
-player5.die();
-player6.die();
-player7.die();
-player8.die();
-*/
-/*
-player1.remove();
-player2.remove();
-player3.die();
-player4.die()
-*/
-
-player1.changeTeam( 'blue' );
-player2.die();
-player3.die();
-player4.die();
+var plane = new Plane();
+plane = new MissileDecorator( plane );
+plane = new AtomDecorator( plane );
+plane.fire();
