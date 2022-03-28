e27d644c64ac0c1239442d4a657dd00c57b76f3e
senbinkop66 <2507019430@qq.com>
2022-03-28 14:51:27 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,28 +1,11 @@
-class RefreshMenuBarCommand implements Command{
-	constructor (){
+var func = function(){
+	outerloop:
+	for ( var i = 0; i < 10; i++ ){
+		innerloop:
+		for ( var j = 0; j < 10; j++ ){
+			if ( i * j >30 ){
+				break outerloop;
+			}
+		}
 	}
-	execute(){
-		console.log( '刷新菜单界面' );
-	}
-}
-
-class AddSubMenuCommand implements Command{
-	constructor (){
-	}
-	execute(){
-		console.log( '增加子菜单' );
-	}
-}
-class DelSubMenuCommand implements Command{
-	constructor (){
-	}
-	// 忘记重写 execute 方法
-}
-
-var refreshMenuBarCommand = new RefreshMenuBarCommand(),
-addSubMenuCommand = new AddSubMenuCommand(),
-delSubMenuCommand = new DelSubMenuCommand();
-
-refreshMenuBarCommand.execute(); // 输出：刷新菜单界面
-addSubMenuCommand.execute(); // 输出：增加子菜单
-delSubMenuCommand.execute(); // 输出：Uncaught TypeError: undefined is not a function
+};
