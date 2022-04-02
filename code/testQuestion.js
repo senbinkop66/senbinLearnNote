3c92c9323aaf8ac9688ddee3733ab5e91abb2f27
senbinkop66 <2507019430@qq.com>
2022-04-02 17:54:36 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,42 +1,14 @@
-/**
- * @param {number[]} arr
- * @return {boolean}
- */
-var canReorderDoubled = function(arr) {
-	let n=arr.length;
-	if (n===0) {
-		return false;
-	}
-	arr.sort((a,b)=>b-a);
-	//console.log(arr);
-	let m=new Map();
-	m.set(arr[0],1);
-	for (let i=1;i<n;i++){
-		//console.log(s);
-		if (arr[i]>=0) {
-			if (m.has(arr[i]*2)) {
-				m.set(arr[i]*2,m.get(arr[i]*2)-1);
-				if (m.get(arr[i]*2)===0) {
-					m.delete(arr[i]*2);
-				}
-			}else{
-				m.set(arr[i],m.has(arr[i]) ? m.get(arr[i])+1 : 1);
-			}
-		}else{
-			if (m.has(arr[i]/2)) {
-				m.set(arr[i]/2,m.get(arr[i]/2)-1);
-				if (m.get(arr[i]/2)===0) {
-					m.delete(arr[i]/2);
-				}
-			}else{
-				m.set(arr[i],m.has(arr[i]) ? m.get(arr[i])+1 : 1);
-			}
-		}
-	}
-	//console.log(m);
-	return m.size===0;
-};
+function currying(fn, n) {
+  return function (m) {
+    return fn.call(this, m, n);
+  };
+}
 
-let arr = [2,1,2,1,1,1,2,2];
-let result=canReorderDoubled(arr);
-console.log(result);
+function tailFactorial(n, total) {
+  if (n === 1) return total;
+  return tailFactorial(n - 1, n * total);
+}
+
+const factorial = currying(tailFactorial, 1);
+
+console.log(factorial(5)); // 120
