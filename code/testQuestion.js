3c35a5a8644135bb973419f0efd878c34cbd106f
senbinkop66 <2507019430@qq.com>
2022-04-03 21:32:05 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,14 +1,28 @@
-let arrayLike = {
-    '0': 'a',
-    '1': 'b',
-    '2': 'c',
-    length: 3
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+	//let n="z".charCodeAt(0)+1;
+	//target=target.charCodeAt(0);
+	if (target>=letters[letters.length-1]) {
+		return letters[0];
+	}
+	let i=0,j=letters.length-1;
+	let mid;
+	while(i<j){
+		mid=i+Math.floor((j-i)/2);
+		if (letters[mid]>target) {
+			j=mid;
+		}else{
+			i=mid+1;
+		}
+	}
+	return letters[i];
+	//String.fromCharCode()
 };
 
-let arr=[].slice.call(arrayLike);
-
-console.log(arr);
-
-let arr2=Array.from(arrayLike);
-
-console.log(arr2)
+let letters = ["c", "f", "j"],target = "c";
+let result=nextGreatestLetter(letters,target);
+console.log(result);
