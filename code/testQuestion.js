fb6b82f9aaf908e64a01c920e6feb010ff7b4548
senbinkop66 <2507019430@qq.com>
2022-04-04 21:35:16 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,28 +1,5 @@
-/**
- * @param {character[]} letters
- * @param {character} target
- * @return {character}
- */
-var nextGreatestLetter = function(letters, target) {
-	//let n="z".charCodeAt(0)+1;
-	//target=target.charCodeAt(0);
-	if (target>=letters[letters.length-1]) {
-		return letters[0];
-	}
-	let i=0,j=letters.length-1;
-	let mid;
-	while(i<j){
-		mid=i+Math.floor((j-i)/2);
-		if (letters[mid]>target) {
-			j=mid;
-		}else{
-			i=mid+1;
-		}
-	}
-	return letters[i];
-	//String.fromCharCode()
-};
+let z={a:3,b:4};
 
-let letters = ["c", "f", "j"],target = "c";
-let result=nextGreatestLetter(letters,target);
-console.log(result);
+let n={...z};
+
+console.log(n);
