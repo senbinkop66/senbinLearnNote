0ca4d3c4894580d31fb91f375c68c4c95fde4939
senbinkop66 <2507019430@qq.com>
2022-04-05 17:02:57 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,5 +1,34 @@
-let z={a:3,b:4};
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var countPrimeSetBits = function(left, right) {
+	let ans=0;
+	for(let i=left;i<=right;i++){
+		if(isPrime(bitCount(i))){
+			ans++;
+		}
+	}
+	return ans;
+};
 
-let n={...z};
+const isPrime=(n)=>{
+	if (n<2) {
+		return false;
+	}
+	for (let i=2;i*i<=n;i++){
+		if (n%i===0) {
+			return false;
+		}
+	}
+	return true;
+}
 
-console.log(n);
+const bitCount=(n)=>{
+	return n.toString(2).split("0").join("").length;
+}
+
+let left = 10, right = 15;
+let result=countPrimeSetBits(left,right);
+console.log(result);
