cb7873b12b756e59144788e534611f90b195e895
senbinkop66 <2507019430@qq.com>
2022-04-06 15:51:05 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,22 +1,4 @@
-/**
- * @param {number[]} height
- * @return {number}
- */
-var maxArea = function(height) {
-    let n=height.length;
-    const container=[];
-    let min;
-    let maxContainer=0;
-    let area=0;
-    for (let i=0;i<n-1;i++){
-        for(let j=1;j<n;j++){
-            area=(j-i)*Math.min(height[i],height[j]);
-            maxContainer=area>maxContainer ? area : maxContainer;
-        }
-    }
-    return maxContainer;
-};
+const x = {};
+x[Symbol.replace] = (...s) => console.log(s);
 
-let height=[1,8,6,2,5,4,8,3,7];
-let result=maxArea(height);
-console.log(result);
+console.log('Hello'.replace(x, 'World')) // ["Hello", "World"]
