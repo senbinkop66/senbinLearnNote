f96dedc02c079f18e91d6e5fe15af4857b950307
senbinkop66 <2507019430@qq.com>
2022-04-16 17:39:15 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,9 +1,26 @@
-let reg1=/\B../g;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var largestPalindrome = function(n) {
+    if (n===1) {
+        return 9;
+    }
+    const upper=10**n-1;
+    for(let left=upper;left>upper/10;left--){
+        let right=String(left).split("").reverse().join("");
+        let p=BigInt(String(left)+right);  //得到回文数
+        let x=BigInt(upper);
+        while(x*x>=p){
+            if (p%x===BigInt(0)) {  //x 是 p 的因子
+                return p%BigInt(1337);
+            }
+            x--;
+        }
 
-let reg2=new RegExp("\\B(ba)","g");
+    }
+};
 
-let str1="baz foot foo";
-let str2=" baz foo aba";
-
-console.log(str1.match(reg1));  //[ 'az', 'oo', 't ', 'oo' ]
-console.log(str2.replace(reg2,"X"));  // baz foo aX
+let n=2;
+let result=largestPalindrome(n);
+console.log(result);
