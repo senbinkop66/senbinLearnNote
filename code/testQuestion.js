0387f3f0db4d63b6b3525c9909a4d838418c99a3
senbinkop66 <2507019430@qq.com>
2022-04-18 12:58:32 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,27 +1,9 @@
-/**
- * @param {string} paragraph
- * @param {string[]} banned
- * @return {string}
- */
-var mostCommonWord = function(paragraph, banned) {
-    paragraph=paragraph.replace(/[!?',;.]/g," ");
-    paragraph=paragraph.toLowerCase().split(" ");
-    let m=new Map();
-    let max=0;
-    let ans="";
-    for (let word of paragraph){
-        if (word!=="" && banned.indexOf(word)===-1) {
-            m.set(word,m.has(word) ? m.get(word)+1 : 1);
-            if (m.get(word)>max){
-                max=m.get(word);
-                ans=word;
-            }
-        }
-    }
-    return ans;
+// 代码示例
+let al2 = {
+    length: 4,
+    '-1': -1,
+    '0': 0,
+    a: 'a',
+    1: 1
 };
-
-let paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
-let banned = ["hit"]
-let result=mostCommonWord(paragraph,banned);
-console.log(result);
+console.log(Array.prototype.slice.call(al2)); //[0, 1, empty × 2]
