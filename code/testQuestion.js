ebabcce923d4736b9b51fb26683330477ff8e0c8
senbinkop66 <2507019430@qq.com>
2022-04-20 12:32:48 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,4 +1,34 @@
+/**
+ * @param {string} s
+ * @param {character} c
+ * @return {number[]}
+ */
+var shortestToChar = function(s, c) {
+    let n=s.length;
+    let ans=new Array(n).fill(0);
+    let leftIndex=s.indexOf(c);
+    let rightIndex=s.indexOf(c,leftIndex+1);
+    let d1,d2;
+    for(let i=0;i<n;i++){
+        d1=Math.abs(i-leftIndex);
+        if (rightIndex!==-1) {
+            d2=Math.abs(i-rightIndex)
+            if (d1<d2) {
+                ans[i]=d1;
+            }else{
+                ans[i]=d2;
+            }
+            if (i===rightIndex) {
+                leftIndex=rightIndex;
+                rightIndex=s.indexOf(c,leftIndex+1);
+            }
+        }else{
+            ans[i]=d1;
+        }
+    }
+    return ans;
+};
 
-setImmediate(()=>{
-    console.log("21")
-});
+let s = "loveleetcode", c = "e";
+let result=shortestToChar(s,c);
+console.log(result);
