334d9e78669ab002ed4d5fcf7967786795847dcd
senbinkop66 <2507019430@qq.com>
2022-04-22 21:33:48 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,4 +1,24 @@
-console.log([]==[]);  // false
-console.log([1]==[1]) //false
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxRotateFunction = function(nums) {
+    let n=nums.length;
+    let sum=0,numSum=0;
+    for(let i=0;i<n;i++){
+        sum+=i*nums[i];
+        numSum+=nums[i];
+        
+    }
+    let maxSum=sum;
+    for (let i=n-1;i>0;i--){
+        //找到迭代公式
+        sum+=numSum-n*nums[i];
+        maxSum=maxSum>sum ? maxSum : sum;
+    }
+    return maxSum;
+};
 
-console.log([]== 0);  // true
+let data = [4,3,2,6];
+let result=maxRotateFunction(data);
+console.log(result);
