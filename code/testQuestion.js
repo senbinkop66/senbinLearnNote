1533936147aa25d831e06fc95f376bcc07700c4c
senbinkop66 <2507019430@qq.com>
2022-04-23 13:53:34 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,34 +1,11 @@
-function mergeSort(arr){
-    //归并排序
-    let n=arr.length;
-    if(n<2){
-        return arr;
-    }
-    let middle=Math.floor(n/2);
-    let left=arr.slice(0,middle);
-    let right=arr.slice(middle);
+/**
+ * @param {number[][]} trees
+ * @return {number[][]}
+ */
+var outerTrees = function(trees) {
 
-    return merge(mergeSort(left),mergeSort(right));
-}
+};
 
-function merge(left,right){
-    let result=[];
-    while(left.length && right.length){
-        if (left[0]<=right[0]) {
-            result.push(left.shift());
-        }else{
-            result.push(right.shift());
-        }
-    }
-    while(left.length){
-        result.push(left.shift());
-    }
-    while(right.length){
-        result.push(right.shift());
-    }
-    return result;
-}
-
-let test=[2,4,1,6,5,8,9,3,7,0];
-let result=mergeSort(test);
+let trees= [[1,1],[2,2],[2,0],[2,4],[3,3],[4,2]];
+let result=outerTrees(trees);
 console.log(result);
