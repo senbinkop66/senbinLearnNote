623f526cc3825b7f360aeee8a766c7d59537e04b
senbinkop66 <2507019430@qq.com>
2022-04-24 17:04:37 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,11 +1,30 @@
-/**
- * @param {number[][]} trees
- * @return {number[][]}
- */
-var outerTrees = function(trees) {
-
-};
-
-let trees= [[1,1],[2,2],[2,0],[2,4],[3,3],[4,2]];
-let result=outerTrees(trees);
-console.log(result);
+class EventEmitter{
+    constructor(){
+        this._events={};
+    }
+    on(event,callback){  //监听event事件，触发时调用callback函数
+        let callbacks=this._events[event] || [];
+        callbacks.push(callback);
+        return this;
+    }
+    off(event,callback){  //停止监听event事件
+        let callbacks=this._events[event];
+        this._events[event]=callbacks && callbacks.filter(fn=>fn!==callback);
+        return this;
+    }
+    emit(...args){  //触发事件，并把参数传给事件的处理函数
+        const event=args[0];
+        const params=[].slice.call(args,1);
+        const callbacks=this._events[event];
+        callbacks.forEach(fn=>fn.apply(this.params));
+        return this;
+    }
+    once(event,callback){  //为事件注册单次监听器
+        let wrapFanc=(...args)=>{
+            callback.apply(this.args);
+            this.off(event,wrapFanc);
+        }
+        this.on(event,wrapFanc);
+        return this;
+    }
+}
