095dc690daaeedd1783c4df036e2f55fa6eb57b3
senbinkop66 <2507019430@qq.com>
2022-04-28 17:25:10 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,55 +1,12 @@
-/**
- * @param {number[][]} heights
- * @return {number[][]}
- */
-const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+var foo=function(){
+    var t=new Date();
+    foo=function(){
+        return t;
+    };
+    return foo();
+}
 
-var pacificAtlantic = function(heights) {
-    let m=heights.length;
-    let n=heights[0].length;
-    const pacific=new Array(m).fill(0).map(()=>new Array(n).fill(0));
-    const atlantic=new Array(m).fill(0).map(()=>new Array(n).fill(0));
-    const dfs=(row,col,ocean)=>{
-        if (ocean[row][col]) {
-            return;
-        }
-        ocean[row][col]=true;
-        for (let dir of dirs){
-            let newRow=row+dir[0];
-            let newCol=col+dir[1];
-            if (newRow>=0 && newRow<m && newCol>=0 && newCol<n && heights[newRow][newCol]>=heights[row][col]) {
-                dfs(newRow,newCol,ocean);
-            }
-        }
-    }
-
-    for (let i=0;i<m;i++){
-        dfs(i,0,pacific);
-    }
-    for (let i=1;i<n;i++){
-        dfs(0,i,pacific);
-    }
-    for (let i=0;i<m;i++){
-        dfs(i,n-1,atlantic);
-    }
-    for (let i=0;i<n-1;i++){
-        dfs(m-1,i,atlantic);
-    }
-
-    const result=[];
-    for (let i=0;i<m;i++){
-        for (let j=0;j<n;j++){
-            if (pacific[i][j] && atlantic[i][j]) {
-                let cell=[];
-                cell.push(i);
-                cell.push(j);
-                result.push(cell);
-            }
-        }
-    }
-    return result;
-};
-
-let heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]];
-let result=pacificAtlantic(heights);
-console.log(result);
+console.log(foo());
+setTimeout(()=>{
+    console.log(foo());
+},2000);
