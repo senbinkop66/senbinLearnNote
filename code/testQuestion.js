035c6ee1532c266a138f394e2b153510288499af
senbinkop66 <2507019430@qq.com>
2022-05-02 21:34:28 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,39 +1,79 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {string} code
+ * @return {boolean}
  */
-/**
- * @param {TreeNode} root1
- * @param {TreeNode} root2
- * @return {number[]}
- */
-var getAllElements = function(root1, root2) {
-    let value1=[];
-    let value2=[]
-    //中序遍历
-    const inorder=(root,ret)=>{
-        if (root) {
-            inorder(root.left,ret);
-            ret.push(root.val);
-            inorder(root.right,ret);
-        }
-    };
-
-    inorder(root1,value1);
-    inorder(root2,value2);
-
-    let ans=[];
-    while(value1.length>0 && value2.length>0){
-        if (value1[0]<value2[0]) {
-            ans.push(value1.shift());
+var isValid = function(code) {
+    let n=code.length;
+    const tags=[];  //存储开始标签
+    let i=0;
+    while (i<n){
+        console.log(code[i]);
+        if (code[i]==="<") {
+            if (i===n-1) {
+                //必需闭合
+                return false;
+            }
+            if (code[i+1]==="/") {
+                //闭合标签时
+                let j=code.indexOf(">",i+2);
+                if (j<0) {
+                    return false;
+                }
+                let tagname=code.slice(i+2,j);  //闭合标签
+                if (tags.length===0 || tags[tags.length-1]!==tagname){
+                    return false;
+                }
+                //匹配则弹出栈顶元素
+                tags.pop();
+                i=j+1;
+                if (tags.length===0 && i!==n) {
+                    return false;
+                }
+            }else if(code[i+1]==="!"){
+                //匹配<![CDATA[]]>
+                if (tags.length===0) {
+                    return false;
+                }
+                if (i+9>n) {
+                    return false;
+                }
+                let cdata=code.slice(i+2,i+9);
+                if (cdata!=="[CDATA[") {
+                    return false;
+                }
+                let j=code.indexOf("]]>",i+9);
+                if (j<0) {
+                    return false;
+                }
+                i=j+1;
+            }else{
+                let j=code.indexOf(">",i+1);
+                if (j<0) {
+                    return false;
+                }
+                let tagname=code.slice(i+1,j);
+                if (tagname.length<1 || tagname.length>9) {
+                    return false;
+                }
+                for (let k=0;k<tagname.length;k++){
+                    if (!(tagname[k]>="A" && tagname[k]<="Z")) {
+                        return false;
+                    }
+                }
+                tags.push(tagname);
+                i=j+1;
+            }
         }else{
-            ans.push(value2.shift());
+            if (tags.length===0) {
+                return false;
+            }
+            i++;
         }
     }
-    ans=ans.concat(value1).concat(value2);
-    return ans;
+    console.log(tags)
+    return tags.length===0;
 };
+
+let code= "<A><A>456</A>  <A> 123  !!  <![CDATA[]]>  123 </A>   <A>123</A></A>";
+let result=isValid(code);
+console.log(result);
