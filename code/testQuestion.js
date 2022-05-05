772d2f7bafb591c60aac9fe1281225883b22e2d1
senbinkop66 <2507019430@qq.com>
2022-05-05 22:12:18 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,31 +1,28 @@
 /**
- * @param {number} n
+ * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
-var findTheWinner = function(n, k) {
-    let arr=new Array(n).fill(0);
-    for (let i=0;i<n;i++){
-        arr[i]=i+1;
-    }
-    let k1;
-    while(arr.length>1){
-        k1=k;
-        if (k>arr.length) {
-            k1=k%arr.length;
-            if (k1===0) {
-                k1=arr.length;
+var numSubarrayProductLessThanK = function(nums, k) {
+    let ans=0;
+    let sum=1;  //连续子数组和
+    let i=0;
+    for (let j=0;j<nums.length;j++){
+        if (nums[j]<k) {
+            while (i<j && sum*nums[j]>=k){
+                sum/=nums[i];
+                i++;
             }
+            sum*=nums[j];
+            ans+=(j-i+1);
+        }else{
+            i=j+1;
+            sum=1;
         }
-
-        for (let i=1;i<k1;i++){
-            arr.push(arr.shift());
-        }
-        arr.shift();
     }
-    return arr[0];
+    return ans;
 };
 
-let n = 6, k = 5;
-let result=findTheWinner(n,k);
+let nums = [100,2,3,4,100,5,6,7,100], k = 100;
+let result=numSubarrayProductLessThanK(nums,k);
 console.log(result);
