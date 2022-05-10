d16a6031d7cd3e0aa7432301d8af59e4e829a011
senbinkop66 <2507019430@qq.com>
2022-05-10 21:25:25 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,27 +1,13 @@
 /**
- * @param {string} s
- * @return {number[]}
+ * @param {string[]} grid
+ * @param {number} catJump
+ * @param {number} mouseJump
+ * @return {boolean}
  */
-var diStringMatch = function(s) {
-    let ans=[];
-    let n=s.length;
-    let left=0,right=n;
-    for (let i=0;i<n;i++){
-        if (s[i]==="I") {
-            //比后一个小则加入当前没有加入最小的的一个
-            ans.push(left);
-            left++;
-        }else if(s[i]==="D"){
-            //比后一个大则加入当前没有加入最大的的一个
-            ans.push(right);
-            right--;
-        }
-    }
-    ans.push(left);  //或 ans.push(right);
-
-    return ans;
+var canMouseWin = function(grid, catJump, mouseJump) {
+    
 };
 
-let s = "DDDII";
-let result=diStringMatch(s)
+let grid = ["####F","#C...","M...."], catJump = 1, mouseJump = 2;
+let result=canMouseWin(grid, catJump, mouseJump);
 console.log(result);
