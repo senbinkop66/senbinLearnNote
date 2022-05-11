ccdd56a3bb5958d070a3c0af9bf876b2d85ca376
senbinkop66 <2507019430@qq.com>
2022-05-11 21:47:16 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,66 +1,21 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
+ * @param {number[]} nums
+ * @return {number}
  */
-
-/**
- * Encodes a tree to a single string.
- *
- * @param {TreeNode} root
- * @return {string}
- */
-var serialize = function(root) {
-    const list=[];
-    const postOrder=(root,list)=>{
-        if (!root) {
-            return;
-        }
-        postOrder(root.left,list);
-        postOrder(root.right,list);
-        list.push(root.val);
+var maxProduct = function(nums) {
+    let n=nums.length;
+    let maxF=nums[0];
+    let minF=nums[0];
+    let ans=nums[0];
+    for (let i=1;i<n;i++){
+        let mx=maxF,mn=minF;
+        maxF=Math.max(mx*nums[i], Math.max(nums[i],mn*nums[i]));
+        ans = Math.max(maxF,ans);
+        minF=Math.min(mn*nums[i], Math.min(nums[i],mx*nums[i]));
     }
-
-    postOrder(root,list);
-
-    const str=list.join(",");
-    return str;
+    return ans;
 };
 
-/**
- * Decodes your encoded data to tree.
- *
- * @param {string} data
- * @return {TreeNode}
- */
-var deserialize = function(data) {
-    if (data.length===0) {
-        return null;
-    }
-    let arr=data.split(",");
-    let n=arr.length;
-    const stack=[];
-    for(let i=0;i<n;i++){
-        stack.push(parseInt(arr[i]));
-    }
-
-    const constructTree=(lower,upper,stack)=>{
-        if (stack.length===0 || stack[stack.length-1]<lower || stack[stack.length-1]>upper) {
-            return null;
-        }
-        let val=stack.pop();
-        const root=new TreeNode(val);
-        root.right=constructTree(val,upper,stack);
-        root.left=constructTree(lower,val,stack);
-        return root;
-    }
-
-    return constructTree(-Number.MAX_SAFE_INTEGER,Number.MAX_SAFE_INTEGER,stack);
-};
-
-/**
- * Your functions will be called as such:
- * deserialize(serialize(root));
- */
+let nums=[2,3,-2,4];
+let result=maxProduct(nums);
+console.log(result);
