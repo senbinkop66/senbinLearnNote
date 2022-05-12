4c5287f457916501cc1e526bb1119a307c866ff6
senbinkop66 <2507019430@qq.com>
2022-05-12 20:22:25 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,52 +1,25 @@
 /**
  * @param {number[]} nums
- * @param {number} k
  * @return {number}
-*/
-var findKthLargest = function(nums, k) {
-    return quickSelect(nums, 0, nums.length - 1, nums.length - k);
-};
+ */
 
-
-var quickSelect=(arr, left, right, index)=>{
-    let q = randomPartition(arr, left, right);  //基准
-    if (q === index) {
-        return arr[q];
-    }else{
-        if (q < index) {
-            return quickSelect(arr, q + 1, right, index);
-        }else{
-            return quickSelect(arr, left, q - 1, index);
-        }
+var rob = function(nums){
+    if (nums.length < 3) {
+        return Math.max(...nums);
     }
-}
 
-//
-var randomPartition=(arr,left,right)=>{
-    let i = Math.floor(Math.random() * (right - left + 1)) + left;
-    swap(arr, i, right);
-    return partition(arr, left, right);
-}
-
-var partition=(arr, left, right)=>{
-    let datum = arr[right];
-    let i = left - 1;
-    for (let j = left; j < right; j++){
-        if (arr[j] <= datum) {
-            swap(arr ,++i, j);
-        }
+    let first = nums[0];
+    let second = Math.max(nums[0],nums[1]);
+    let i = 2;
+    while(i < nums.length){
+        let temp = second;
+        second = Math.max(second, first + nums[i]);
+        first = temp;
+        i++;
     }
-    swap(arr, i+1, right);
-    return i+1;
+    return second;
 }
 
-//交换元素
-var swap=(arr,i,j)=>{
-    let temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
-}
-
-let test=[3,2,3,1,2,4,5,5,6];
-
-console.log(findKthLargest(test,4));  //
+let nums=[2,7,9,3,1];
+let result=rob(nums);
+console.log(result);
