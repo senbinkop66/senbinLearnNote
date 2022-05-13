e3364f9896bdf56bb8ac13c8fe1e763ad2f96090
senbinkop66 <2507019430@qq.com>
2022-05-13 16:31:03 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,55 +1,41 @@
-function TreeNode(val, left, right){
-    this.val = (val === undefined ? 0 : val);
-    this.left = (left === undefined ? null : left);
-    this.right = (right === undefined ? null : right);
-}
-
 /**
- * @param {TreeNode} root1
- * @param {TreeNode} root2
- * @return {TreeNode}
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
  */
-
-var mergeTrees = function(root1, root2) {
-    if (root1 == null){
-        return root2;
+var smallestK = function(arr, k) {
+    if (k === 0) {
+        return [];
     }
-    if (root2 == null) {
-        return root1;
+    if (arr.length === k) {
+        return arr;
     }
-    const root3 = new TreeNode(root1.val + root2.val);
-    const queue1 = [root1];
-    const queue2 = [root2];
-    const queue3 = [root3];
-
-    while (queue1.length > 0 && queue2.length > 0){
-        let node3 = queue3.pop();
-        let node1 = queue1.pop();
-        let node2 = queue2.pop();
-        if (node1.left !== null || node2.left !== null) {
-            if (node1.left !== null && node2.left !== null) {
-                node3.left = new TreeNode(node1.left.val + node2.left.val);
-                queue1.push(node1.left);
-                queue2.push(node2.left);
-                queue3.push(node3.left);
-            }else if (node1.left !== null){
-                node3.left = node1.left;
-            }else if (node2.left !== null){
-                node3.left = node2.left;
-            }
-        }
-        if (node1.right !== null || node2.right !== null) {
-            if (node1.right !== null && node2.right !== null) {
-                node3.right = new TreeNode(node1.right.val + node2.right.val);
-                queue1.push(node1.right);
-                queue2.push(node2.right);
-                queue3.push(node3.right);
-            }else if (node1.right !== null){
-                node3.right = node1.right;
-            }else if (node2.right !== null){
-                node3.right = node2.right;
-            }
+    const left = [], right = [], same = [];
+    let randonIndex = Math.floor(arr.length/2);
+    let pivolt = arr[randonIndex];
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] < pivolt) {
+            left.push(arr[i]);
+        }else if (arr[i] > pivolt){
+            right.push(arr[i]);
+        }else{
+            same.push(arr[i]);
         }
     }
-    return root3;
+    if (left.length === k) {
+        return left;
+    }else if (left.length > k){
+        return smallestK(left,k);
+    }else if (left.length < k) {
+        let res = left.length + same.length;
+        if (res >= k) {
+            return left.concat(same.slice(0, k - left.length))
+        }else{
+            return left.concat(same, smallestK(right, k - left.length - same.length));
+        }
+    }
 };
+
+let arr = [1,3,5,7,2,4,6,8], k = 4;
+let result = smallestK(arr, k);
+console.log(result);
