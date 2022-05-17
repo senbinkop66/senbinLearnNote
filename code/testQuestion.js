b7f24fda303f82131e9c66950669343d81a253d4
senbinkop66 <2507019430@qq.com>
2022-05-17 22:20:57 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,29 +1,11 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
+ * @param {string} s
+ * @return {boolean}
  */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var swapPairs = function(head) {
-    if (head === null || head.next === null) {
-        return head;
-    }
-    let dummyHead = new ListNode(0);
-    dummyHead.next = head;
-
-    let temp = dummyHead;
-    while (temp.next !== null && temp.next.next !== null){
-        let node1 = temp.next;
-        let node2 = temp.next.next;
-        temp.next = node2;
-        node1.next = node2.next;
-        node2.next = node1;
-        temp = node1;
-    }
-    return dummyHead.next;
+var repeatedSubstringPattern = function(s) {
+   return (s + s).indexOf(s, 1) !== s.length;
 };
+
+let s = "abcabcabcabc";
+let result = repeatedSubstringPattern(s);
+console.log(result);
