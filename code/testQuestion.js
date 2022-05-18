b563a253f737e643d9c24637f45d2790e9f64975
senbinkop66 <2507019430@qq.com>
2022-05-18 13:25:50 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,11 +1,27 @@
 /**
- * @param {string} s
- * @return {boolean}
+ * @param {number} m
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
  */
-var repeatedSubstringPattern = function(s) {
-   return (s + s).indexOf(s, 1) !== s.length;
+var findKthNumber = function(m, n, k) {
+    let left = 1;
+    let right = m * n;
+    while (left < right){
+        let x = left + Math.floor((right - left) /2);
+        let count = Math.floor(x / n) * n;
+        for (let i = Math.floor(x / n) + 1; i <= m; i++){
+            count += Math.floor(x / i);
+        }
+        if (count >= k) {
+            right = x;
+        }else{
+            left = x + 1;
+        }
+    }
+    return left;
 };
 
-let s = "abcabcabcabc";
-let result = repeatedSubstringPattern(s);
+let  m = 3, n = 3, k = 5;
+let result = findKthNumber(m, n, k);
 console.log(result);
