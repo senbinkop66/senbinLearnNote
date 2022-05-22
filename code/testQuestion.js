5e2e08314318c8da1859d2390c2ead50a7b34c2c
senbinkop66 <2507019430@qq.com>
2022-05-22 15:14:43 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,11 +1,30 @@
 /**
- * @param {string} word
+ * @param {number} maxChoosableInteger
+ * @param {number} desiredTotal
  * @return {boolean}
  */
-var detectCapitalUse = function(word) {
-    let reg = /(^[A-Z]+$)|(^[A-Z][a-z]+$)|(^[a-z]+$)/g;
-    return reg.test(word);
+var canIWin = function(maxChoosableInteger, desiredTotal) {
+    let minChoosableInteger = 1;
+    var dfs = function(minChoosableInteger, maxChoosableInteger, desiredTotal){
+            if (maxChoosableInteger >= desiredTotal) {
+                return true;
+            }
+            if (minChoosableInteger + maxChoosableInteger < desiredTotal) {
+                minChoosableInteger++;
+                maxChoosableInteger--;
+                desiredTotal -= minChoosableInteger + maxChoosableInteger;
+                dfs(minChoosableInteger, maxChoosableInteger, desiredTotal);
+            }else{
+                return false;
+            }
+    }
+    if ((1 + maxChoosableInteger) * maxChoosableInteger / 2 < desiredTotal) {
+        return false;
+    }
+    return dfs(minChoosableInteger, maxChoosableInteger, desiredTotal)
 };
-let word = "FlaG";
-let result = detectCapitalUse(word);
+
+
+let maxChoosableInteger = 10, desiredTotal = 0;
+let result = canIWin(maxChoosableInteger, desiredTotal);
 console.log(result);
