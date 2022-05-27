d8010bb3d78f39279716591b38f62908c215950c
senbinkop66 <2507019430@qq.com>
2022-05-27 21:25:40 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,28 +1,11 @@
 /**
- * @param {string[]} words
- * @param {string} word1
- * @param {string} word2
+ * @param {number[]} nums
  * @return {number}
  */
-var findClosest = function(words, word1, word2) {
-    let ans = Number.MAX_SAFE_INTEGER;
-    let index1 = -1;
-    let index2 = -1;
-    words.forEach((word, index) => {
-        if (word === word1 || word === word2) {
-            if (word === word1){
-                index1 = index;
-            }else{
-                index2 = index;
-            }
-            if (index1 !== -1 && index2 !== -1) {
-                ans = Math.min(Math.abs(index1 - index2), ans);
-            }
-        }
-    });
-    return ans;
+var findLHS = function(nums) {
+    
 };
 
-let words = ["I","am","a","student","from","a","university","in","a","city"], word1 = "a", word2 = "student";
-let result = findClosest(words, word1, word2);
+let nums = [1,3,2,2,5,2,3,7];
+let result = findLHS(nums);
 console.log(result);
