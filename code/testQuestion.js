a9c62b0d03f3587c3165e3fa9696f1c692fbae17
senbinkop66 <2507019430@qq.com>
2022-05-27 18:29:29 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,17 +1,28 @@
 /**
- * @param {number[]} candyType
+ * @param {string[]} words
+ * @param {string} word1
+ * @param {string} word2
  * @return {number}
  */
-var distributeCandies = function(candyType) {
-    let s = new Set();
-    let n = candyType.length;
-    for (let i =0; i < n; i++){
-        s.add(candyType[i]);
-    }
-    let candyTypeNum = s.size;
-    if (n / 2 >= candyTypeNum) {
-        return candyTypeNum;
-    }else{
-        return n / 2;
-    }
+var findClosest = function(words, word1, word2) {
+    let ans = Number.MAX_SAFE_INTEGER;
+    let index1 = -1;
+    let index2 = -1;
+    words.forEach((word, index) => {
+        if (word === word1 || word === word2) {
+            if (word === word1){
+                index1 = index;
+            }else{
+                index2 = index;
+            }
+            if (index1 !== -1 && index2 !== -1) {
+                ans = Math.min(Math.abs(index1 - index2), ans);
+            }
+        }
+    });
+    return ans;
 };
+
+let words = ["I","am","a","student","from","a","university","in","a","city"], word1 = "a", word2 = "student";
+let result = findClosest(words, word1, word2);
+console.log(result);
