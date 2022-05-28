094b3ba8cd31f4603bf458d4fcf03c83a08fa578
senbinkop66 <2507019430@qq.com>
2022-05-28 08:26:12 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,11 +1,28 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * @param {string} s
+ * @return {string}
  */
-var findLHS = function(nums) {
-    
+var removeOuterParentheses = function(s) {
+    let stack = [];
+    let ans = "";
+    for (let i = 0; i < s.length; i++){
+        let c = s[i];
+        if (c === ")") {
+            stack.pop();
+        }
+        //在遇到 ‘)’ 并将栈顶字符出栈后，如果栈为空，则不把字符放入结果
+        if (stack.length){
+            //其他情况下，需要把字符放入结果
+            ans += c;
+        }
+        //在遇到 ‘(’ 并将字符入栈后，如果栈的深度为 1，则不把字符放入结果
+        if (c === "(") {
+            stack.push(c);
+        }
+    }
+    return ans;
 };
 
-let nums = [1,3,2,2,5,2,3,7];
-let result = findLHS(nums);
+let s = "(()())(())";
+let result = removeOuterParentheses(s);
 console.log(result);
