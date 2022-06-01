ca1ca3fab07f24c6f49270120adcb4196e7aaacc
senbinkop66 <2507019430@qq.com>
2022-06-01 16:47:51 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,79 +1,44 @@
 /**
- * @param {string[]} words
- * @return {string}
+ * @param {number[]} matchsticks
+ * @return {boolean}
  */
-var alienOrder = function(words) {
-    const VISITING = 1, VISITED = 2;
-    let valid = true;
-    const edges = new Map();
-    const states = new Map();
-    const length = words.length;
-    for (const word of words) {
-        const wordLength = word.length;
-        for (let j = 0; j < wordLength; j++) {
-            const c = word[j];
-            if (!edges.has(c)) {
-                edges.set(c, []);
+var makesquare = function(matchsticks) {
+    let len = matchsticks.length;
+    if (len < 4) {
+        return false;
+    }
+    let totalLen = 0;
+    matchsticks.forEach((item) => {
+        totalLen += item;
+    });
+    if (totalLen % 4 !== 0) {
+        return false;
+    }
+    matchsticks.sort((a, b) => b - a);
+    let edgeLength = totalLen / 4;
+    if (matchsticks[0] > edgeLength) {
+        return false;
+    }
+    const edges = new Array(4).fill(0);
+
+    const dfs = (index) => {
+        if (index === len) {
+            return true;
+        }
+        for (let i = 0; i < 4; i++){
+            edges[i] += matchsticks[index];
+            if (edges[i] <= edgeLength && dfs(index + 1)) {
+                return true;
             }
+            edges[i] -= matchsticks[index];
         }
+        return false;
     }
+    return dfs(0);
 
-    const addEdge = (before, after) => {
-        const length1 = before.length, length2 = after.length;
-        const length = Math.min(length1, length2);
-        let index = 0;
-        while (index < length) {
-            const c1 = before[index], c2 = after[index];
-            if (c1 !== c2) {
-                edges.get(c1).push(c2);
-                break;
-            }
-            index++;
-        }
-        if (index === length && length1 > length2) {
-            valid = false;
-        }
-    }
-
-    const dfs = (u) => {
-        states.set(u, VISITING);
-        const adjacent = edges.get(u);
-        for (const v of adjacent) {
-            if (!states.has(v)) {
-                dfs(v);
-                if (!valid) {
-                    return;
-                }
-            } else if (states.get(v) === VISITING) {
-                valid = false;
-                return;
-            }
-        }
-        states.set(u, VISITED);
-        order[index] = u;
-        index--;
-    }
-
-    for (let i = 1; i < length && valid; i++) {
-        addEdge(words[i - 1], words[i]);
-    }
-    const order = new Array(edges.size).fill(0);
-    let index = edges.size - 1;
-    const letterSet = edges.keys();
-    for (const u of letterSet) {
-        if (!states.has(u)) {
-            dfs(u);
-        }
-    }
-    if (!valid) {
-        return "";
-    }
-    return order.join('');
-
-    
 };
 
 
-let words = ["wrt","wrf","er","ett","rftt"];
-let result = alienOrder(words);
+let matchsticks = [1,1,2,2,2];
+let result = makesquare(matchsticks);
 console.log(result);
