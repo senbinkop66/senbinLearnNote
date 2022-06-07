f2760ebd36094d4e469b40a1434306059fe76c5f
senbinkop66 <2507019430@qq.com>
2022-06-07 11:09:34 +0800

    update

--- Vue/vue3.md
+++ Vue/vue3.md
@@ -200,7 +200,7 @@ Vue.createApp({
 
 # 介绍
 
-Vue (读音 /vjuː/，类似于 **view**) 是一套用于构建用户界面的**渐进式框架**。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与[现代化的工具链](https://v3.cn.vuejs.org/guide/single-file-component.html)以及各种[支持类库](https://github.com/vuejs/awesome-vue#components--libraries)结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。
+Vue (读音 /vjuː/，类似于 **view**) 是一套用于构建用户界面的**渐进式框架**。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。**Vue 的核心库只关注视图层**，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与[现代化的工具链](https://v3.cn.vuejs.org/guide/single-file-component.html)以及各种[支持类库](https://github.com/vuejs/awesome-vue#components--libraries)结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。
 
 ## 声明式渲染
 
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,29 +1,24 @@
-/**
- * @param {number} radius
- * @param {number} x_center
- * @param {number} y_center
- */
-var Solution = function(radius, x_center, y_center) {
-    this.radius = radius;
-    this.x_center = x_center;
-    this.y_center = y_center;
+var MyCalendarThree = function() {
+    this.tree = new Map();
+    this.lazy = new Map();
 };
 
-/**
- * @return {number[]}
- */
-Solution.prototype.randPoint = function() {
-    while (true){
-        let x = Math.random() * (this.radius * 2) - this.radius;
-        let y = Math.random() * (this.radius * 2) - this.radius;
-        if (x * x + y * y <= this.radius * this.radius) {
-            return [this.x_center + x, this.y_center + y];
-        }
+MyCalendarThree.prototype.book = function(start, end) {
+    this.update(start, end - 1, 0, 1000000000, 1);
+    return this.tree.get(1) || 0;
+};
+
+MyCalendarThree.prototype.update = function(start, end, l, r, idx) {
+    if (r < start || end < l) {
+        return;
+    } 
+    if (start <= l && r <= end) {
+        this.tree.set(idx, (this.tree.get(idx) || 0) + 1);
+        this.lazy.set(idx, (this.lazy.get(idx) || 0) + 1);
+    } else {
+        const mid = (l + r) >> 1;
+        this.update(start, end, l, mid, 2 * idx);
+        this.update(start, end, mid + 1, r, 2 * idx + 1);
+        this.tree.set(idx, (this.lazy.get(idx) || 0) + Math.max((this.tree.get(2 * idx) || 0), (this.tree.get(2 * idx + 1) || 0)));
     }
-};
-
-/**
- * Your Solution object will be instantiated and called as such:
- * var obj = new Solution(radius, x_center, y_center)
- * var param_1 = obj.randPoint()
- */
+}
