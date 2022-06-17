f41054d0469f455670e1178736e83e7020c64fda
senbinkop66 <2507019430@qq.com>
2022-06-17 22:34:45 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,23 +1,36 @@
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
  */
-var findPairs = function(nums, k) {
-   const visited = new Set();
-   const res = new Set();
-    for (let num of nums) {
-        if (visited.has(num - k)) {
-            res.add(num - k);
+var duplicateZeros = function(arr) {
+    let n = arr.length;
+    let top = 0;
+    let i = -1;
+    while (top < n) {
+        i++;
+        if (arr[i] !== 0) {
+            top++;
+        } else {
+            top += 2;
         }
-        if (visited.has(num + k)) {
-            res.add(num);
-        }
-        visited.add(num);
     }
-    return res.size;
+    let j = n - 1;
+    if (top === n + 1) {
+        arr[j] = 0;
+        j--;
+        i--;
+    }
+    while (j >= 0) {
+        arr[j] = arr[i];
+        j--;
+        if (arr[i] === 0) {
+            arr[j] = 0;
+            j--;
+        }
+        i--;
+    }
 };
 
-let nums = [3, 1, 4, 1, 5], k = 2;
-let result = findPairs(nums, k);
-console.log(result);
+let arr = [0,4,1,0,0,8,0,0,3];
+duplicateZeros(arr);
+console.log(arr);
