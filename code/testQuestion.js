d475ee3c8bd7b611f7caaf216483bd508de3b63b
senbinkop66 <2507019430@qq.com>
2022-06-22 11:26:13 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,7 +1,27 @@
 /**
- * @param {string} address
- * @return {string}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var defangIPaddr = function(address) {
-    return address.replace(/\./g, "[.]");
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function(head) {
+    if (head.next === null) {
+        return null;
+    }
+    let slow = head;
+    let fast = head;
+    let prev = null;
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        prev = slow;
+        slow = slow.next;
+    }
+
+    prev.next = prev.next.next;
+    return head;
 };
