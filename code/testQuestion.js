e35a36cb9a0b2222851b385e2e02c006b8c19a71
senbinkop66 <2507019430@qq.com>
2022-07-24 22:06:19 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,23 +1,23 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {number[]} distance
+ * @param {number} start
+ * @param {number} destination
+ * @return {number}
  */
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
-var pruneTree = function(root) {
-    if (!root) {
-        return null;
+var distanceBetweenBusStops = function(distance, start, destination) {
+    if (start > destination) {
+        [start, destination] = [destination, start]
     }
-    root.left = pruneTree(root.left);
-    root.right = pruneTree(root.right);
-    if (!root.left && !root.right && root.val === 0) {
-        return null;
+    let sum1 = 0, sum2 = 0;
+    for (let i = 0; i < distance.length; i++) {
+        if (i >= start && i < destination) {
+            sum1 += distance[i];
+        } else {
+            sum2 += distance[i];
+        }
     }
-    return root;
+    return Math.min(sum1, sum2);
 };
+
+let distance = [1,2,3,4], start = 0, destination = 1;
+console.log(distanceBetweenBusStops(distance, start, destination));
