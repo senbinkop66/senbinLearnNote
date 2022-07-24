0ffffb54d8b333ffefc3236456629c6bdef2d6ff
senbinkop66 <2507019430@qq.com>
2022-07-24 21:51:34 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,14 +1,23 @@
-function countPI(n) {
-    let pi = 0;
-    for (let i = 1; i <= n; i++) {
-        if (i % 2 === 0) {
-            pi -= 1/(i * 2 - 1)
-        } else {
-            pi += 1/(i * 2 - 1)
-        }
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var pruneTree = function(root) {
+    if (!root) {
+        return null;
     }
-    return pi * 4;
-}
-
-let PI = countPI(100000);
-console.log(PI);
+    root.left = pruneTree(root.left);
+    root.right = pruneTree(root.right);
+    if (!root.left && !root.right && root.val === 0) {
+        return null;
+    }
+    return root;
+};
