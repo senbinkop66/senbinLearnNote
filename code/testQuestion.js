6daf233b8d80961db8cbe7baeadf0c1f60094d71
senbinkop66 <2507019430@qq.com>
2022-07-30 21:08:39 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,27 +1,17 @@
 /**
- * @param {number[]} height
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
  * @return {number}
  */
-var trap = function(height) {
-    //动态规划方法
-    let n = height.length;
-    let leftMax = new Array(n);
-    let rightMax = new Array(n);
-
-    leftMax[0] = height[0];
-    rightMax[n - 1] = height[n - 1];
-
-    for (let i = 1; i < n; i++) {
-        leftMax[i] = Math.max(leftMax[i - 1], height[i]);
-        rightMax[n - 1 - i] = Math.max(rightMax[n - i], height[n - 1 - i]);
+var maxCount = function(m, n, ops) {
+    let mina = m, minb = n;
+    for (const op of ops) {
+        mina = Math.min(mina, op[0]);
+        minb = Math.min(minb, op[1]);
     }
-
-    let ans = 0;
-    for (let i = 0; i < n; i++) {
-        ans += Math.min(leftMax[i], rightMax[i]) - height[i];
-    }
-    return ans;
+    return mina * minb;
 };
 
-let height = [0,1,0,2,1,0,1,3,2,1,2,1];
-console.log(trap(height));
+let m = 3, n = 3, ops = [[2,2],[3,3]];
+console.log(maxCount(m, n, ops));
