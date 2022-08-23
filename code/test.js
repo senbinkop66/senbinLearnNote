93c78384bcd7fbd768bcca00b5b5dde059aead10
senbinkop66 <2507019430@qq.com>
2022-08-23 21:53:55 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,17 +1,65 @@
-var a = 'window';
-var obj1 = {
-    a: 1,
-    fn1: function () {
-        var _this = this;
-        return function () { return console.log(_this.a); };
+/*
+ * function ListNode(x){
+ *   this.val = x;
+ *   this.next = null;
+ * }
+ */
+
+/**
+ * 
+ * @param head ListNode类 the head node
+ * @return ListNode类
+ */
+function sortInList( head ) {
+    // write code here
+    return toSortList(head, null);
+};
+
+const toSortList = (head, tail) => {
+    if (head === null) {
+        return head;
     }
+    if (head.next === tail) {
+        head.next = null;
+        return head;
+    }
+    let slow = head, fast = head;
+    while (fast !== tail) {
+        slow = slow.next;
+        fast = fast.next;
+        if (fast !== tail) {
+            fast = fast.next;
+        }
+    }
+    const mid = slow;
+    return merge(toSortList(head, mid), toSortList(mid, tail));
+}
+
+const merge = (head1, head2) => {
+    if (head1 === null) {
+        return head2;
+    }
+    if (head2 === null) {
+        return head1;
+    }
+
+    const dummyNode = new ListNode(-1);
+    let pre = dummyNode;
+    while (head1 !== null && head2 !== null) {
+        if (head1.val <= head2.val) {
+            pre.next = head1;
+            head1 = head1.next;
+        } else {
+            pre.next = head2;
+            head2 = head2.next;
+        }
+        pre = pre.next;
+    }
+    pre.next = head1 === null ? head2 : head1;
+
+    return dummyNode.next;
+}
+
+module.exports = {
+    sortInList : sortInList
 };
-var obj2 = {
-    a: 2
-};
-
-obj1.fn1()(); // 1
-
-obj1.fn1().call(obj2); // 1 
-
-obj1.fn1.call(obj2)(); // 2
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,20 +1,78 @@
-/*function ListNode(x){
-    this.val = x;
-    this.next = null;
-}*/
-function FindFirstCommonNode(pHead1, pHead2)
-{
-    // write code here
-    if (pHead1 === null || pHead2 === null) {
-        return null;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    if (head === null) {
+        return head;
     }
-    let p1 = pHead1, p2 = pHead2;
-    while (p1 !== p2) {
-        p1 = p1 === null ? pHead2 : p1.next;
-        p2 = p2 === null ? pHead1 : p2.next;
+    let len = 0;
+    let node = head;
+    while (node !== null) {
+        len++;
+        node = node.next;
     }
-    return p1;
-}
-module.exports = {
-    FindFirstCommonNode : FindFirstCommonNode
+
+    const dummyNode = new ListNode(-1, head);
+
+    for (let subLen = 1; subLen < len; subLen <<= 1) {
+        let prev = dummyNode, curr = dummyNode.next;
+        while (curr !== null) {
+            let head1 = curr;
+            for (let i = 1; i < subLen && curr.next !== null; i++) {
+                curr = curr.next;
+            }
+            let head2 = curr.next;
+            curr.next = null;
+            curr = head2;
+            for (let i = 1; i < subLen && curr !== null && curr.next !== null; i++) {
+                curr = curr.next;
+            }
+            let next = null;
+            if (curr !== null) {
+                next = curr.next;
+                curr.next = null;
+            }
+            const merged = merge(head1, head2);
+            prev.next = merged;
+            while (prev.next !== null) {
+                prev = prev.next;
+            }
+            curr = next;
+        }
+    }
+    return dummyNode.next;
 };
+
+
+const merge = (head1, head2) => {
+    if (head1 === null) {
+        return head2;
+    }
+    if (head2 === null) {
+        return head1;
+    }
+
+    const dummyNode = new ListNode(-1);
+    let pre = dummyNode;
+    while (head1 !== null && head2 !== null) {
+        if (head1.val <= head2.val) {
+            pre.next = head1;
+            head1 = head1.next;
+        } else {
+            pre.next = head2;
+            head2 = head2.next;
+        }
+        pre = pre.next;
+    }
+    pre.next = head1 === null ? head2 : head1;
+
+    return dummyNode.next;
+}
