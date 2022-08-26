69c294a74691cf040e0b2af67cca32119e209f5a
senbinkop66 <2507019430@qq.com>
2022-08-26 23:46:51 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,25 +1,36 @@
 /**
  * @param {number[]} nums
- * @return {number}
+ * @return {number[][]}
  */
-var firstMissingPositive = function(nums) {
+var permuteUnique = function(nums) {
+	nums.sort((a, b) => a -b);  // 排序
 	const n = nums.length;
-	for (let i = 0; i < n; i++) {
-		while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
-			// 把1-n的元素放到对应的位置
-			let temp = nums[nums[i] - 1];
-			nums[nums[i] - 1] = nums[i];
-			nums[i] = temp;
-		}
-	}
+	const ans = [];
+	const visited = new Array(n).fill(false);
 
-	for (let i = 0; i < n; i++) {
-		if (nums[i] !== i + 1) {
-			return i + 1;
+	const backtrack = (idx, perm) => {
+		// 所有数都填完了
+		if (idx === n) {
+			ans.push([...perm]);
+			return;
+		}
+		for (let i = 0; i < n; i++) {
+			if (visited[i] || (i > 0 && nums[i] === nums[i -1] && !visited[i - 1])) {
+				continue;
+			}
+			perm.push(nums[i]);
+			visited[i] = true;
+			// 继续递归填下一个数
+			backtrack(idx + 1, perm);
+			// 撤销操作
+			visited[i] = false;
+			perm.pop();
 		}
 	}
-	return n + 1;
+	backtrack(0, []);
+
+	return ans;
 };
 
-let nums = [3,4,-1,1];
-console.log(firstMissingPositive(nums));
+let nums = [3,4,1,1];
+console.log(permute(nums));
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -3,49 +3,27 @@
  * @param num int整型一维数组 
  * @return int整型二维数组
  */
-function threeSum( nums ) {
-    nums = nums.sort((a, b) => a - b);  //排序
-    let n = nums.length;
+function permute( nums ) {
+    const n = nums.length;
     const ans = [];
-    let sum = 0;
-    //枚举第一个数字
-    for (let first = 0; first < n; first++){
-        // 需要和上一次枚举的数不相同
-        if (nums[first] > 0) {
-            //first元素大于0后，后面sum不可能等于0
-            break;
+    const backtrack = (first) => {
+        // 所有数都填完了
+        if (first === n) {
+            ans.push([...nums]);
         }
-        if (first > 0 && nums[first] === nums[first - 1]){
-            continue;
-        }
-        //第三个数对应的指针初始指向数组的最右端
-        let third = n - 1;
-        let target = -nums[first];
-        for (let second = first + 1; second < n; second++){
-            if (nums[second] > target) {
-                //前两个元素和已经大于0，则再加第三个一定大于0;
-                break;
-            }
-            // 需要和上一次枚举的数不相同
-            if (second > first + 1 && nums[second] === nums[second - 1]) {
-                continue;
-            }
-            // 需要保证 second 的指针在 third 的指针的左侧
-            while(second < third && nums[second] + nums[third] > target){
-                third--;
-            }
-            // 如果指针重合，随着 second 后续的增加
-            // 就不会有满足 a+b+c = 0 并且 b<c 的 c 了，可以退出循环
-            if (second === third) {
-                break;
-            }
-            if (nums[second] + nums[third] === target) {
-                ans.push([nums[first], nums[second], nums[third]]);
-            }
+        for (let i = first; i < n; i++) {
+            // 动态维护数组
+            [nums[first], nums[i]] = [nums[i], nums[first]];
+            // 继续递归填下一个数
+            backtrack(first + 1);
+            // 撤销操作
+            [nums[i], nums[first]] = [nums[first], nums[i]];
         }
     }
+    backtrack(0);
+
     return ans;
 }
-module.exports  =  {
-    threeSum : threeSum
+module.exports = {
+    permute : permute
 };
