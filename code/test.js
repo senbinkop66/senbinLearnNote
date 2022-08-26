0f000188a4ea8669852065ea49f19fc25d4318c4
senbinkop66 <2507019430@qq.com>
2022-08-26 22:31:21 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,11 +1,25 @@
-function Insert(num)
-{
-    // write code here
-}
-function GetMedian(){
-	// write code here
-}
-module.exports = {
-    Insert : Insert,
-    GetMedian : GetMedian
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+	const n = nums.length;
+	for (let i = 0; i < n; i++) {
+		while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+			// 把1-n的元素放到对应的位置
+			let temp = nums[nums[i] - 1];
+			nums[nums[i] - 1] = nums[i];
+			nums[i] = temp;
+		}
+	}
+
+	for (let i = 0; i < n; i++) {
+		if (nums[i] !== i + 1) {
+			return i + 1;
+		}
+	}
+	return n + 1;
 };
+
+let nums = [3,4,-1,1];
+console.log(firstMissingPositive(nums));
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,22 +1,51 @@
 /**
- * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
- * 返回表达式的值
- * @param s string字符串 待计算的表达式
- * @return int整型
+ * 
+ * @param num int整型一维数组 
+ * @return int整型二维数组
  */
-function solve( s ) {
-    // write code here
-    s = s.replace(/\s/, "");
-    const nums = [];
-    const ops = [];
-    //设定运算符优先级
-    const m = new Map([["(", -1], ["+", 1], ["-", 1], ["*", 2]]);
-    
-    
+function threeSum( nums ) {
+    nums = nums.sort((a, b) => a - b);  //排序
+    let n = nums.length;
+    const ans = [];
+    let sum = 0;
+    //枚举第一个数字
+    for (let first = 0; first < n; first++){
+        // 需要和上一次枚举的数不相同
+        if (nums[first] > 0) {
+            //first元素大于0后，后面sum不可能等于0
+            break;
+        }
+        if (first > 0 && nums[first] === nums[first - 1]){
+            continue;
+        }
+        //第三个数对应的指针初始指向数组的最右端
+        let third = n - 1;
+        let target = -nums[first];
+        for (let second = first + 1; second < n; second++){
+            if (nums[second] > target) {
+                //前两个元素和已经大于0，则再加第三个一定大于0;
+                break;
+            }
+            // 需要和上一次枚举的数不相同
+            if (second > first + 1 && nums[second] === nums[second - 1]) {
+                continue;
+            }
+            // 需要保证 second 的指针在 third 的指针的左侧
+            while(second < third && nums[second] + nums[third] > target){
+                third--;
+            }
+            // 如果指针重合，随着 second 后续的增加
+            // 就不会有满足 a+b+c = 0 并且 b<c 的 c 了，可以退出循环
+            if (second === third) {
+                break;
+            }
+            if (nums[second] + nums[third] === target) {
+                ans.push([nums[first], nums[second], nums[third]]);
+            }
+        }
+    }
+    return ans;
 }
-
-let s = "(2*(3-4))*5";
-
-module.exports = {
-    solve : solve
+module.exports  =  {
+    threeSum : threeSum
 };
