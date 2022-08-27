36efcd6d4f3ef2a7a879bf26486ed8af6f456dba
senbinkop66 <2507019430@qq.com>
2022-08-27 21:20:33 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,36 +1,26 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var permuteUnique = function(nums) {
-	nums.sort((a, b) => a -b);  // 排序
-	const n = nums.length;
-	const ans = [];
-	const visited = new Array(n).fill(false);
+function (nums, n) {
+	let evenMax = 0;
+	let oddMax = 0；
+	for (let i = 0; i < n; ++i) {
+			let num = nums[i]
+      if (i % 2 === 0) {
+      	evenMax = evenMax > num ? evenMax : num;
+      } else {
+      	oddMax = oddMax > num ? oddMax : num;
+      }
+  }
 
-	const backtrack = (idx, perm) => {
-		// 所有数都填完了
-		if (idx === n) {
-			ans.push([...perm]);
-			return;
-		}
-		for (let i = 0; i < n; i++) {
-			if (visited[i] || (i > 0 && nums[i] === nums[i -1] && !visited[i - 1])) {
-				continue;
-			}
-			perm.push(nums[i]);
-			visited[i] = true;
-			// 继续递归填下一个数
-			backtrack(idx + 1, perm);
-			// 撤销操作
-			visited[i] = false;
-			perm.pop();
-		}
-	}
-	backtrack(0, []);
-
-	return ans;
-};
-
-let nums = [3,4,1,1];
-console.log(permute(nums));
+  let res = 0
+  for (let i = 0; i < n; ++i) {
+      if (i % 2 === 0) {
+          res += (evenMax - nums[i]);
+      }
+      else {
+          res += (oddMax - nums[i]);
+      }
+  }
+  if (evenMax == oddMax){
+  	res += (n / 2);
+  }
+  return ans;
+}
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,29 +1,31 @@
-/**
- * 
- * @param num int整型一维数组 
- * @return int整型二维数组
- */
-function permute( nums ) {
-    const n = nums.length;
+var permutation = function(s) {
+    const n = s.length;
     const ans = [];
-    const backtrack = (first) => {
-        // 所有数都填完了
-        if (first === n) {
-            ans.push([...nums]);
+    const visited = new Array(n).fill(false);
+
+    const arr = Array.from(s).sort();  //首先对原字符串排序，保证相同的字符都相邻
+    const perm = [];
+
+    const backtrack = (arr, i, n, perm) => {
+        if (i === n) {
+            ans.push(perm.join(""));
+            return;
         }
-        for (let i = first; i < n; i++) {
-            // 动态维护数组
-            [nums[first], nums[i]] = [nums[i], nums[first]];
-            // 继续递归填下一个数
-            backtrack(first + 1);
-            // 撤销操作
-            [nums[i], nums[first]] = [nums[first], nums[i]];
+        for (let j = 0; j < n; j++) {
+            if (visited[j] || (j > 0 && !visited[j - 1] && arr[j - 1] === arr[j])) {
+                continue;  // 避免重复
+            }
+            visited[j] = true;
+            perm.push(arr[j]);
+            backtrack(arr, i + 1, n, perm);
+            perm.pop();
+            visited[j] = false;
         }
     }
-    backtrack(0);
-
+    backtrack(arr, 0, n, perm);
+    const size = ans.length;
     return ans;
 }
-module.exports = {
-    permute : permute
-};
+
+let s = "abc";
+console.log(permutation(s))
