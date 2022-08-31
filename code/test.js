527a75a18393e15fc2535e07ee05d486e045aba4
senbinkop66 <2507019430@qq.com>
2022-08-31 14:45:14 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,20 +1,36 @@
 /**
- * @param {number[]} coins
- * @param {number} amount
+ * @param {number[]} nums
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-	if (amount === 0) {
+var lengthOfLIS = function(nums) {
+	if (nums.length === 0) {
 		return 0;
 	}
-	const dp = new Array(amount + 1).fill(Infinity);
-	dp[0] = 0;
-	const n = coins.length;
-	for (let i = 0; i < coins.length; i++) {
-		for (let j = coins[i]; j <= amount; j++) {
-			dp[j] = Math.min(dp[j - coins[i]] + 1, dp[j]);
+	const n = nums.length;
+	const dp = new Array(n + 1).fill(0);
+	let len = 1;
+	dp[len] = nums[0];
+
+	for (let i = 1; i < n; i++) {
+		if (nums[i] > dp[len]) {
+			dp[++len] = nums[i];
+		} else {
+			// 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+			let left = 1, right = len, pos = 0;
+			while (left <= right) {
+				const mid = (left + right) >> 1;
+				if (dp[mid] < nums[i]) {
+					pos = mid;
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			}
+			dp[pos + 1] = nums[i];
 		}
 	}
+	return len;
+};
 
-	return dp[amount] === Infinity ? -1 : dp[amount];
-}
+let nums = [10,9,2,5,3,7,101,18];
+console.log(lengthOfLIS(nums))
