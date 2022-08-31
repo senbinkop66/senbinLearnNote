43a31f6bd5ef1df58c214499dd4c62c1ba11f9b9
senbinkop66 <2507019430@qq.com>
2022-08-31 16:55:26 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,36 +1,61 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 
+ * @param A string字符串 
+/**
+ * @param {string} s
+ * @return {string}
  */
-var lengthOfLIS = function(nums) {
-	if (nums.length === 0) {
-		return 0;
-	}
-	const n = nums.length;
-	const dp = new Array(n + 1).fill(0);
-	let len = 1;
-	dp[len] = nums[0];
+var longestPalindrome  =  function(s) {
+    const n = s.length;
+    if (n < 2) {
+        return s;
+    }
 
-	for (let i = 1; i < n; i++) {
-		if (nums[i] > dp[len]) {
-			dp[++len] = nums[i];
-		} else {
-			// 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
-			let left = 1, right = len, pos = 0;
-			while (left <= right) {
-				const mid = (left + right) >> 1;
-				if (dp[mid] < nums[i]) {
-					pos = mid;
-					left = mid + 1;
-				} else {
-					right = mid - 1;
-				}
-			}
-			dp[pos + 1] = nums[i];
-		}
-	}
-	return len;
+    let maxLen = 1;
+    let begin = 0;
+    const dp = new Array(n).fill(0).map(() => new Array(n).fill(false));
+    //dp[i][j] 表示 s[i..j] 是否是回文串
+    for (let i = 0; i < n; i++){
+        /// 初始化：所有长度为 1 的子串都是回文串
+        dp[i][i] = true;
+    }
+
+    // 递推开始, 先枚举子串长度
+    for(let L = 2; L <= n; L++){
+        // 枚举左边界，左边界的上限设置可以宽松一些
+        for(let i = 0; i < n; i++){
+            // 由 L 和 i 可以确定右边界，即 j - i + 1  =  L 得
+            let j = L + i - 1;
+            if (j >= n) {
+                // 如果右边界越界，就可以退出当前循环
+                break;
+            }
+            if (s[i] !== s[j]) {
+                dp[i][j] = false;
+            } else {
+                if (j - i < 3) {
+                    dp[i][j] = true;
+                } else {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+            }
+            // 只要 dp[i][L]  =  =  true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+            if (dp[i][j] && j - i + 1 > maxLen) {
+                maxLen = j - i + 1;
+                begin = i;
+            }
+        }
+    }
+
+    return s.slice(begin, begin + maxLen);
 };
 
-let nums = [10,9,2,5,3,7,101,18];
-console.log(lengthOfLIS(nums))
+
+let str1  =  "babad";
+let result = longestPalindrome(str1);
+console.log(result);
+module.exports  =  {
+    getLongestPalindrome : getLongestPalindrome
+};
