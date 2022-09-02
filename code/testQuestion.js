a942a280379472b24b22e9e3bb4c0635dc7f25ff
senbinkop66 <2507019430@qq.com>
2022-09-02 22:16:50 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,10 +1,17 @@
-var str = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-function generateMixed(n) {
-     var res = "";
-     for(var i = 0; i < n ; i ++) {
-         var id = Math.ceil(Math.random()*35);
-         res += str[id];
-     }
-     return res;
-}
-console.log(generateMixed(6));
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  const n = prices.length;
+  if (n < 2) {
+    return 0;
+  }
+  const dp = new Array(n).fill(0).map(() => new Array(2).fill(0));
+  dp[0][0] = 0, dp[0][1] = -prices[0];
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+  }
+  return dp[n - 1][0];
+};
