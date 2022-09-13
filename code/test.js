95626a384445e6d8ef8edc5b42a24002d08c7ca5
senbinkop66 <2507019430@qq.com>
2022-09-13 22:12:17 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,22 +1,3 @@
-function Super(){
-    this.name=["super"];
-}
-Super.prototype.getSuper=function(){
-    return this.name;
-}
-
-function Sub(){
+function solution(head) {
 
 }
-
-Sub.prototype = new Super();  //将Sub的原型对象Sub.prototype指向Super的实例
-
-var sub1=new Sub();  //创建Sub的实例sub1
-sub1.name.push("sub1");
-
-console.log(sub1.getSuper());  // [ 'super', 'sub1' ]
-
-var sub2=new Sub();  //创建Sub的实例sub2
-sub2.name.push("sub2");
-
-console.log(sub2.getSuper());  //[ 'super', 'sub1', 'sub2' ]
