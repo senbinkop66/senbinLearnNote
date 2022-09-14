3382ade8889ae4cb1c33af61f0ca1715a8decdf4
senbinkop66 <2507019430@qq.com>
2022-09-14 17:25:27 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,3 +1,52 @@
-function solution(head) {
-
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+var getLeastNumbers = function(arr, k) {
+    randomizedSelected(arr, 0, arr.length - 1, k);
+    return arr.slice(0, k);
 }
+
+function randomizedSelected(arr, left, right, k) {
+    if (left > right) {
+        return;
+    }
+    const pos = randomizedPartition(arr, left, right);
+    const num = pos - left + 1;
+    if (k === num) {
+        return;
+    } else if (k < num) {
+        //表示第 k 小的数在 pivot 的左侧
+        randomizedSelected(arr, left, pos - 1, k);
+    } else {
+        // 表示第 k 小的数在 pivot 的右侧
+        randomizedSelected(arr, pos + 1, right, k);
+    }
+}
+
+function randomizedPartition(arr, left, right) {
+    const pivotIndex = left + Math.floor(Math.random() * (right - left));
+    swap(arr, right, pivotIndex);
+    return partition(arr, left, right);
+}
+
+function partition(arr, left, right) {
+    const pivot = arr[right];
+    let i = left - 1;
+    for (let j = left; j <= right - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, right);
+    return i + 1;
+}
+
+function swap(arr, i, j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+let nums = [4, 5, 2, 7, 9, 1, 0, 3, 8, 6];
+console.log(getLeastNumbers(nums, 5))
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,23 +1,2 @@
-function solution(arr) {
-    let result = {};
-    const n = arr.length;
-    for (let i = 0; i < n; i++) {
-        let date = arr[i].split("-");
-        if (date.length === 3) {
-            let ym = date[0]+"-" + date[1];
-            if (result[ym]) {
-                if (result[ym].indexOf(date[2]) == -1) {
-                    result[ym].push(date[2]);
-                }
-            } else {
-                result[ym] = [date[2]];
-            }
-        }
-    }
-    return result;
-}
-
-const input=['2022-8-1','2022-8-1','2022-8-2','2022-9-1']
-let ouput = solution(input);
-
-console.log(ouput);
+let arr = [1, 2, 4, 5];
+console.log(arr.slice(0, 2))
