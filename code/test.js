b195cbb405b14c0d152e3e9a122d2d7af56a38ca
senbinkop66 <2507019430@qq.com>
2022-09-18 17:06:03 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,32 +1,9 @@
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("success");
-    console.log("timer1");
-  }, 1000);
-  console.log("promise1里的内容");
-});
+function runAsync (x) {
+    const p = new Promise(r => setTimeout(() => r(x, console.log(x)), 1000))
+    return p
+}
 
-const promise2 = promise1.then(() => {
-  throw new Error("error!!!");
-});
-
-console.log("promise1", promise1);
-console.log("promise2", promise2);
-
-setTimeout(() => {
-  console.log("timer2");
-  console.log("promise1", promise1);
-  console.log("promise2", promise2);
-}, 2000);
-
-
+runAsync(2);
 /*
-promise1里的内容
-promise1 Promise { <pending> }
-promise2 Promise { <pending> }
-timer1
-Uncaught (in promise) Error: error!!!
-timer2
-promise1 Promise { 'success' }
-promise2 Promise { <rejected>: Error: error!!! }
+2
 */
