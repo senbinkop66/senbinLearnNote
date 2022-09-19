0b0147155028cd1fd9d386ce8cc4bfd3eeb412c5
senbinkop66 <2507019430@qq.com>
2022-09-19 21:52:07 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,21 +1,47 @@
-const p1 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve('resolve3');
-    console.log('timer1')
-  }, 0)
-  resolve('resovle1');
-  resolve('resolve2');
-}).then(res => {
-  console.log(res)
-  setTimeout(() => {
-    console.log(p1)
-  }, 1000)
-}).finally(res => {
-  console.log('finally', res)
-})
+// M 队列长度，N 传输通道数，P 每个通道大小
+// Q 每个数据包大小，S 每个数据包传输时长
+function  solution(M, N, P, Q, S) {
+    if (N === 1) {
+        return S.reduce((pre, cur) => pre + cur);
+    }
+    let minCost = 0;
+    
+    P = P.sort((a, b) => a - b);
+    S = S.map((item, index) => [item, index]);
+    S = S.sort((a, b) => b[0] - a[0]);
+    console.log(S);
 
-/*
-resovle1
-finally undefined
+    const dp = new Array(N).fill(0);
 
-*/
+    for (let i = 0; i < M; i++) {
+        let flag = false;
+        let minIndex = 0;
+        for (let j = 0; j < N; j++) {
+            if (!flag && P[j] >= Q[S[i][1]]) {
+                flag = true;
+                minIndex = j;
+            }
+            if (flag) {
+                if (dp[j] === 0) {
+                    minIndex = j;
+                    // dp[j] += S[i][0];
+                    // minCost = Math.max(minCost, dp[j]);
+                    break;
+                }
+                if (dp[j] < dp[minIndex]) {
+                    minIndex = j;
+                }
+            }
+        }
+        dp[minIndex] += S[i][0];
+        minCost = Math.max(minCost, dp[minIndex]);
+    }
+    return minCost;
+}
+
+let [M, N] = [6, 3];
+let P = [1, 2, 4];
+let Q = [2, 3, 2, 4, 1, 3];
+let S = [3, 4, 1, 5, 3, 2];
+
+console.log(solution(M, N, P, Q, S));
--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,5 +1,39 @@
-let num = 1234.567800;
+// M 生命, N 吊桥长度，K 缺失木板数，L 缺失编号
+function solution(M, N, K, L) {
+    const bridge = new Array(N + 2).fill(true);  // // 记录陷阱
+    L.forEach((item) => {
+        bridge[item] = false;
+    });
+    
+    // dp[i][j]表示到达位置i，剩余生命值为j的方法数
+    const dp = new Array(N + 2).fill(0).map((item) => new Array(M + 2).fill(0));
+    // 初始化dp,在初始位置，剩余生命值为M
+    dp[0][M] = 1;
 
-console.log(num.toPrecision(3))  // 1.23e+3
-console.log(num.toFixed(3))  // 1234.568
-console.log(Math.round(num));  //1235
+    for (let i = 1; i < N + 2; i++) {
+        for (let j = 1; j < M + 1; j++) {
+            // 如果当前位置为陷阱，则需要上一位置多一条命，到当前位置扣一条命
+            let k = bridge[i] === false ? (j + 1) : j;
+            if (i === 1) {
+                dp[i][j] = dp[i - 1][k];
+            } else if (i === 2) {
+                dp[i][j] = dp[i - 1][k] + dp[i - 2][k];
+            } else {
+                dp[i][j] = dp[i - 1][k] + dp[i - 2][k] + dp[i - 3][k];
+            }
+        }
+    }
+
+    // dp[N+1][0~M]相加
+    let ans = 0;
+    for (let i = 0; i < M + 1; i++) {
+        ans += dp[N + 1][i];
+    }
+
+    return ans;
+}
+
+let M = 3, N = 30, K = 8;
+let L = [2, 3, 6, 8, 9, 12, 18, 26];
+
+console.log(solution(M, N, K, L));
