2c571eb4155f15645e10de85be1a4dc1cc674d31
senbinkop66 <2507019430@qq.com>
2022-09-21 17:16:39 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,6 +1,40 @@
-console.log(false.toString());  //  false
-console.log((1).toString());  //  1
-let a = 1;
-console.log(a.toString());  // 1
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    const rows = new Array(9).fill(0).map(() => new Array(9).fill(0));
+    const columns = new Array(9).fill(0).map(() => new Array(9).fill(0));
+    const subboxes = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => new Array(9).fill(0)));
 
-console.log(1.toString());  //  SyntaxError: Invalid or unexpected token
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const c = board[i][j];
+            if (c !== ".") {
+                const index = Number(c) - 1;
+                rows[i][index]++;
+                columns[j][index]++;
+                subboxes[Math.floor(i / 3)][Math.floor(j / 3)][index]++;
+                if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[Math.floor(i / 3)][Math.floor(j / 3)][index] > 1) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+};
+
+
+let board = 
+    [["5","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]];
+
+let result = isValidSudoku(board);
+console.log(result);
