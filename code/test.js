d77414c910266c538662e3b28ebb267baafe3b71
senbinkop66 <2507019430@qq.com>
2022-09-21 22:18:35 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,40 +1,29 @@
 /**
- * @param {character[][]} board
- * @return {boolean}
+ * @param {string[]} strs
+ * @return {string[][]}
  */
-var isValidSudoku = function(board) {
-    const rows = new Array(9).fill(0).map(() => new Array(9).fill(0));
-    const columns = new Array(9).fill(0).map(() => new Array(9).fill(0));
-    const subboxes = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => new Array(9).fill(0)));
-
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            const c = board[i][j];
-            if (c !== ".") {
-                const index = Number(c) - 1;
-                rows[i][index]++;
-                columns[j][index]++;
-                subboxes[Math.floor(i / 3)][Math.floor(j / 3)][index]++;
-                if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[Math.floor(i / 3)][Math.floor(j / 3)][index] > 1) {
-                    return false;
-                }
-            }
+var groupAnagrams = function(strs) {
+    const n = strs.length;
+    const ans = [];
+    const words = new Map();
+    for (let i = 0; i < n; i++) {
+        let word = strs[i].split("");
+        word.sort();
+        word = word.join("");
+        // console.log(word)
+        if (words.has(word)) {
+            words.get(word).push(strs[i]);
+        } else {
+            words.set(word, [strs[i]]);
         }
     }
-    return true;
+    for (let [key, value] of words) {
+        ans.push(value);
+    }
+    return ans;
 };
 
 
-let board = 
-    [["5","3",".",".","7",".",".",".","."]
-    ,["6",".",".","1","9","5",".",".","."]
-    ,[".","9","8",".",".",".",".","6","."]
-    ,["8",".",".",".","6",".",".",".","3"]
-    ,["4",".",".","8",".","3",".",".","1"]
-    ,["7",".",".",".","2",".",".",".","6"]
-    ,[".","6",".",".",".",".","2","8","."]
-    ,[".",".",".","4","1","9",".",".","5"]
-    ,[".",".",".",".","8",".",".","7","9"]];
-
-let result = isValidSudoku(board);
+let strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+let result = groupAnagrams(strs);
 console.log(result);
