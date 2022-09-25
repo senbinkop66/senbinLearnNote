0ce348de371b66a89c3c95f7d3f5a8ecaccfe216
senbinkop66 <2507019430@qq.com>
2022-09-25 20:55:42 +0800

    update

--- code/testQuestion.js
+++ code/testQuestion.js
@@ -1,11 +1,37 @@
-var a = 10;
-var obj = {
-  a: 20
+// 环形字符串的最长相同字符子串
+function solution(s) {
+    const n = s.length;
+    if (n < 2) {
+        return n;
+    }
+    let maxLen = 1;
+    let count = 1;
+    for (let i = 1; i < n; i++) {
+        if (s[i] === s[i - 1]) {
+            count++;
+        } else {
+            maxLen = Math.max(maxLen, count);
+            count = 1;
+        }
+    }
+    maxLen = Math.max(maxLen, count);
+
+    if (n === maxLen) {
+        return maxLen;
+    }
+
+    if (s[0] === s[n - 1]) {
+        count++;
+        let i = 1;
+        while (s[i] === s[i - 1]) {
+            i++;
+            count++;
+        }
+    }
+    maxLen = Math.max(maxLen, count);
+
+    return maxLen;
 }
 
-function fn() {
-  this = obj; // 修改this，运行后会报错
-  console.log(this.a);
-}
-
-fn();  // SyntaxError: Invalid left-hand side in assignment
+let s = 'abaaabaaaba';
+console.log(solution(s))
