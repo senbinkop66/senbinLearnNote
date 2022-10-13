1ba8efb652b35a28f71aae9aea5adef2d6e5bddd
senbinkop66 <2507019430@qq.com>
2022-10-13 16:05:36 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,28 +1,18 @@
 /**
- * @param {string} s1
- * @param {string} s2
- * @return {boolean}
+ * @param {number[]} arr
+ * @return {number}
  */
-var areAlmostEqual = function(s1, s2) {
-  const n = s1.length;
-  const diff = [];
+var maxChunksToSorted = function(arr) {
+  const n = arr.length;
+  let ans = 0, maxValue = 0;
   for (let i = 0; i < n; i++) {
-    if (s1[i] !== s2[i]) {
-      if (diff.length >= 2) {
-        return false;
-      }
-      diff.push(i)
+    maxValue = Math.max(maxValue, arr[i]);
+    if (maxValue === i) {
+      ans++;
     }
   }
-  if (diff.length === 0) {
-    return true;
-  }
-  if (diff.length !== 2) {
-    return false;
-  }
-
-  return s1[diff[0]] === s2[diff[1]] && s1[diff[1]] === s2[diff[0]];
+  return ans;
 };
 
-let s1 = "attack", s2 = "defend";
-console.log(areAlmostEqual(s1, s2));
+let arr = [1,0,2,3,4];
+console.log(maxChunksToSorted(arr));
