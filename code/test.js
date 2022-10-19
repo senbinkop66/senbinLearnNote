16d98841891eeb6753b3c57b97fc46f3659f0b06
senbinkop66 <2507019430@qq.com>
2022-10-19 13:06:04 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,29 +1,26 @@
 /**
- * @param {string[]} digits
- * @param {number} n
+ * @param {number[]} students
+ * @param {number[]} sandwiches
  * @return {number}
  */
-var atMostNGivenDigitSet = function(digits, n) {
-  const s = '' + n;
-  const m = digits.length, k = s.length;
-  const dp = new Array(k + 1).fill(0).map(() => new Array(2).fill(0));
-  dp[0][1] = 1;
-  for (let i = 1; i <= k; i++) {
-    for (let j = 0; j < m; j++) {
-      if (digits[j][0] === s[i - 1]) {
-        dp[i][1] = dp[i - 1][1];
-      } else if (digits[j][0] < s[i - 1]) {
-        dp[i][0] += dp[i - 1][1];
-      } else {
-        break;
-      }
-    }
-    if (i > 1) {
-      dp[i][0] += m + dp[i - 1][0] * m;
+var countStudents = function(students, sandwiches) {
+  const n = students.length;
+  let sum1 = 0;
+  students.forEach((item) => {
+    sum1 += item;
+  });
+  let sum0 = n - sum1;
+  for (let i = 0; i < n; i++) {
+    if (sandwiches[i] === 0 && sum0 > 0) {
+      sum0--;
+    } else if (sandwiches[i] === 1 && sum1 > 0) {
+      sum1--;
+    } else {
+      break;
     }
   }
-  return dp[k][0] + dp[k][1];
+  return sum0 + sum1;
 };
 
-let digits = ["1","4","9"], n = 1000000000;
-console.log(atMostNGivenDigitSet(digits, n));
+let students = [1,1,0,0], sandwiches = [0,1,0,1];
+console.log(countStudents(students, sandwiches));
