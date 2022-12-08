e6ea769710432d48524fa1665341a36afa114bca
senbinkop66 <2507019430@qq.com>
2022-12-08 20:15:39 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,17 +1,8 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var minOperations = function(s) {
-  let cnt = 0;
-  for (let i = 0; i < s.length; i++) {
-    const c = s[i];
-    if (c !== (String.fromCharCode('0'.charCodeAt() + i % 2))) {
-      cnt++;
-    }
-  }
-  return Math.min(cnt, s.length - cnt);
-};
 
-let s = "0100";
-console.log(minOperations(s));
+function randomFen() {
+  let a = Math.floor(Math.random() * 100 / 20);
+  let b = Math.floor(Math.random() * 100 / 20);
+  console.log(a + ":" + b)
+}
+
+randomFen()
